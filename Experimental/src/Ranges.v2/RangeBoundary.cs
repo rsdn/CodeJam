f659d28d0745dbea7ca2b23411cf9b98b8533023
ig-sinicyn <igor.sinicyn@gmail.com>
2016-04-20 14:43:28 +0300

    Exprimental - ranges v2, helper types

---
+++ Experimental/src/Ranges.v2/RangeBoundary.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Ranges.v2
+{
+	/// <summary>
+	/// RangeBoundary helpers and extension methods
+	/// </summary>
+	[PublicAPI]
+	public static class RangeBoundary
+	{
+		#region Helper types
+		/// <summary>
+		/// Implementation of order-by-descending comparer.
+		/// </summary>
+		public sealed class DescendingComparer<T>: IComparer<RangeBoundary<T>>
+		{
+			#region IComparer<Range<T>>
+			/// <summary>
+			/// Order-by-descending comparison
+			/// </summary>
+			public int Compare(RangeBoundary<T> x, RangeBoundary<T> y) => y.CompareTo(x);
+			#endregion
+		}
+		#endregion
+
+		#region Constants
+		internal const string EmptyString = "∅";
+
+		internal const string NegativeInfinityBoundaryString = "(-∞";
+		internal const string PositiveInfinityBoundaryString = "+∞)";
+
+		internal const char FromExclusiveChar = '(';
+		internal const char FromInclusiveChar = '[';
+		internal const char ToExclusiveChar = ')';
+		internal const char ToInclusiveChar = ']';
+		#endregion
+
+		/// <summary>
+		/// Checks that boundary1 is complementation for boundary1 .
+		/// </summary>
+		/// <typeparam name="T"> The type of the boundary value </typeparam>
+		/// <param name="boundary">The range boundary</param>
+		/// <param name="other">Another boundary</param>
+		/// <returns><c>True</c>, if boundary1 is complementation for boundary1.</returns>
+		public static bool IsComplementationFor<T>(this RangeBoundary<T> boundary, RangeBoundary<T> other) => 
+			boundary.HasValue && boundary.GetComplementation() == other;
+
+		/// <summary>
+		/// Returns complementation for the boundary. The conversions are:
+		/// * 'a]' -> '(a'
+		/// * '[a' -> 'a)'
+		/// * 'a)' -> '[a'
+		/// * '(a' -> 'a]' 
+		/// Empty or infinite boundaries will throw. Check <see cref="RangeBoundary{T}.HasValue"/>
+		/// before calling the method.
+		/// </summary>
+		/// <typeparam name="T"> The type of the boundary value </typeparam>
+		/// <param name="boundary">The range boundary</param>
+		/// <returns>Complementation for the boundary</returns>
+		public static RangeBoundary<T> GetComplementation<T>(this RangeBoundary<T> boundary)
+		{
+			RangeBoundaryKind newKind;
+			switch (boundary.Kind)
+			{
+				case RangeBoundaryKind.ToExclusive:
+					newKind = RangeBoundaryKind.FromInclusive;
+					break;
+				case RangeBoundaryKind.FromInclusive:
+					newKind = RangeBoundaryKind.ToExclusive;
+					break;
+				case RangeBoundaryKind.ToInclusive:
+					newKind = RangeBoundaryKind.FromExclusive;
+					break;
+				case RangeBoundaryKind.FromExclusive:
+					newKind = RangeBoundaryKind.ToInclusive;
+					break;
+				default:
+					throw CodeExceptions.UnexpectedArgumentValue(nameof(boundary.Kind), boundary.Kind);
+			}
+
+			var value = boundary.Value;
+			return new RangeBoundary<T>(value, newKind);
+		}
+
+		/// <summary>
+		/// Creates a new boundary with a new value.
+		/// If the boundary has on value the value will not update.
+		/// </summary>
+		/// <typeparam name="T"> The type of the boundary value </typeparam>
+		/// <param name="boundary">Граница диапазона.</param>
+		/// <param name="updateCallback">Callback для нового значения границы.</param>
+		/// <returns>Граница с тем же типом, но обновлённым значением.</returns>
+		public static RangeBoundary<T> UpdateValue<T>(
+			this RangeBoundary<T> boundary,
+			[NotNull, InstantHandle] Func<T, T> updateCallback)
+		{
+			if (boundary.HasValue)
+			{
+				var newValue = updateCallback(boundary.Value);
+
+				if (newValue == null)
+				{
+					boundary = boundary.IsFromBoundary ?
+						RangeBoundary<T>.NegativeInfinity :
+						RangeBoundary<T>.PositiveInfinity;
+				}
+				else
+				{
+					boundary = new RangeBoundary<T>(newValue, boundary.Kind);
+				}
+			}
+
+			return boundary;
+		}
+	}
+}
---
+++ Experimental/src/Ranges.v2/RangeBoundaryKind.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeJam.Ranges.v2
+{
+	/// <summary>
+	/// The kind of range boundary.
+	/// </summary>
+	// DONTTOUCH: the values and the order of the members is important.
+	// DONTTOUCH: DO NOT mark as [Flags]. This is single-value enum.
+	public enum RangeBoundaryKind : byte
+	{
+		/// <summary>Empty,                    '∅'.</summary>
+		Empty = 0x0,
+		/// <summary>Negative infinity,        '(-∞,??',  no From limit.</summary>
+		NegativeInfinity = 0x1,
+		/// <summary>Less than value,          '??,b)',   x &lt; b.</summary>
+		ToExclusive = 0x2,
+		/// <summary>Greater than or equal to, '[a,??',   x >= a.</summary>
+		FromInclusive = 0x4,
+		/// <summary>Less than or equal to,    '??,b]',   x &lt;= b.</summary>
+		ToInclusive = 0x8,
+		/// <summary>Greater than value,       '(a,??',   x > a.</summary>
+		FromExclusive = 0x10,
+		/// <summary>PositiveInfinity,         '??,+∞)',  no To limit.</summary>
+		PositiveInfinity = 0x20,
+	}
+}
