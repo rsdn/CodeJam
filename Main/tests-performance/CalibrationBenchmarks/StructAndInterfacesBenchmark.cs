0548ad15e1d1e6ad3d7edff278d1734bb6a0fa01
ig-sinicyn <igor.sinicyn@gmail.com>
2016-04-20 10:14:46 +0300

    Perftest to proof there's effective way to build effective fluent API over structs

---
+++ Main/tests-performance/CalibrationBenchmarks/StructAndInterfacesBenchmark.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+using BenchmarkDotNet.NUnit;
+
+using JetBrains.Annotations;
+
+using NUnit.Framework;
+
+using static CodeJam.AssemblyWideConfig;
+
+namespace CodeJam
+{
+	/// <summary>
+	/// Proof test: there's effective way to use interfaces over struct
+	/// Used as a proof that the fluent API over extension methods will be effective enough
+	/// to see what I'm talking about: https://blogs.msdn.microsoft.com/alexj/2009/07/31/another-c-trick-fluents-inheritance-and-extension-methods/
+	/// </summary>
+	[TestFixture(Category = BenchmarkConstants.BenchmarkCategory + ": Self-testing")]
+	[PublicAPI]
+	[SuppressMessage("ReSharper", "SuggestVarOrType_BuiltInTypes")]
+	[SuppressMessage("ReSharper", "SuggestVarOrType_SimpleTypes")]
+	public class StructAndInterfacesBenchmark
+	{
+		[Test]
+		[Explicit(BenchmarkConstants.ExplicitExcludeReason)]
+		public void BenchmarkStructAndInterfaces() =>
+			CompetitionBenchmarkRunner.Run(this, RunConfig);
+
+		public const int Count = 10 * 1000 * 1000;
+
+		[CompetitionBaseline]
+		public int Test00Baseline()
+		{
+			int a = 0;
+			for (int i = 0; i < Count; i++)
+			{
+				a = a + 1;
+			}
+			return Count;
+		}
+
+		[CompetitionBenchmark(0.97, 1.08)]
+		public int Test01Struct()
+		{
+			int a = 0;
+			Struct s = new Struct();
+			for (int i = 0; i < Count; i++)
+			{
+				a = s.Call(a);
+			}
+			return Count;
+		}
+
+		[CompetitionBenchmark(0.99, 1.07)]
+		public int Test02StructInterfaceCall()
+		{
+			int a = 0;
+			Struct s = new Struct();
+			for (int i = 0; i < Count; i++)
+			{
+				a = s.CallInterface(a);
+			}
+			return Count;
+		}
+
+		[CompetitionBenchmark(22.72, 24.82)]
+		public int Test03InterfaceCall()
+		{
+			int a = 0;
+			Struct s = new Struct();
+			for (int i = 0; i < Count; i++)
+			{
+				a = CallExt(s, a);
+			}
+			return Count;
+		}
+
+		[CompetitionBenchmark(1.95, 2.09)]
+		public int Test04GenericCall()
+		{
+			int a = 0;
+			Struct s = new Struct();
+			for (int i = 0; i < Count; i++)
+			{
+				a = CallGeneric(s, a);
+			}
+			return Count;
+		}
+
+		[CompetitionBenchmark(1.96, 2.09)]
+		public int Test05GenericStructCall()
+		{
+			int a = 0;
+			Struct s = new Struct();
+			for (int i = 0; i < Count; i++)
+			{
+				a = CallGenericStruct(s, a);
+			}
+			return Count;
+		}
+
+		private struct Struct : IStruct
+		{
+			public int Call(int a) => a + 1;
+
+			public int CallInterface(int a) => a + 1;
+		}
+
+		private interface IStruct
+		{
+			int CallInterface(int a);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static int CallExt(IStruct s, int a) => s.CallInterface(a);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static int CallGeneric<TStruct>(TStruct s, int a)
+			where TStruct : IStruct => s.CallInterface(a);
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static int CallGenericStruct<TStruct>(TStruct s, int a)
+			where TStruct : struct, IStruct => s.CallInterface(a);
+	}
+}
