550421cad4bc1beb5d369ad6139f79de328d7a5d
ig-sinicyn <igor.sinicyn@gmail.com>
2016-04-29 23:44:37 +0300

    Fn - Action & Func factory methods

---
+++ Main/tests/FnTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+using CodeJam.Arithmetic;
+using CodeJam.Strings;
+
+using NUnit.Framework;
+
+// ReSharper disable once CheckNamespace
+
+namespace CodeJam
+{
+	[TestFixture]
+	public class FnTests
+	{
+		[Test]
+		public void TestFn()
+		{
+			Assert.IsFalse(Fn.False());
+			Assert.IsTrue(Fn.True());
+		}
+
+		[Test]
+		public void TestAction()
+		{
+			var i = 0;
+			var a = Fn.Action(() => i++);
+
+			Assert.AreEqual(i, 0);
+			a();
+			Assert.AreEqual(i, 1);
+			a();
+			Assert.AreEqual(i, 2);
+		}
+
+		[Test]
+		public void TestFunc()
+		{
+			var i = 0;
+			var f = Fn.Func(() => i++);
+
+			Assert.AreEqual(i, 0);
+			f();
+			Assert.AreEqual(i, 1);
+			f();
+			Assert.AreEqual(i, 2);
+		}
+	}
+}
