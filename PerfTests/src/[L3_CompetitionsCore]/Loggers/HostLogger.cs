bf022133c55468f2ecb145c7a7ca7d86c3d83ff3
ig-sinicyn <igor.sinicyn@gmail.com>
2016-05-26 20:17:33 +0300

    * CodeJam.BenchmarkDotNet project - WORK IN PROGRESS
    Phase 5: Final naming & refactoring changes, part 3.
    * Host logger code review & tests.

--- PerfTests/src/[L3_CompetitionsCore]/Loggers/HostLogger.cs
+++ PerfTests/src/[L3_CompetitionsCore]/Loggers/HostLogger.cs
@@ -4,51 +4,103 @@ using System.Threading;
 
 using BenchmarkDotNet.Loggers;
 
+using CodeJam.Threading;
+
 using JetBrains.Annotations;
 
 namespace CodeJam.PerfTests.Loggers
 {
+	/// <summary>
+	/// Basic logger implementation for unit test runners
+	/// </summary>
 	[PublicAPI]
 	[SuppressMessage("ReSharper", "SuggestVarOrType_BuiltInTypes")]
+	[SuppressMessage("ReSharper", "IntroduceOptionalParameters.Global")]
 	public class HostLogger : ILogger
 	{
-		// DONTTOUCH: Verify that all code uses these constants before changing
-		// DONTTOUCH: Check that PreprocessLine works after changing
-		// TODO: unittest for that
+		// DONTTOUCH: Check that all code does not hardcode content of the constants before changing
+
+		#region Log line prefixes (constants)
+		/// <summary>
+		/// The prefix for informational log lines.
+		/// Lines with this prefix will be written even if <seealso cref="DetailedLogging"/> set to <c>false</c>.
+		/// </summary>
 		public const string LogInfoPrefix = "// ?";
+
+		/// <summary>
+		/// The prefix for important log lines.
+		/// Lines with this prefix will be written even if <seealso cref="DetailedLogging"/> set to <c>false</c>.
+		/// </summary>
 		public const string LogImportantInfoPrefix = "// !";
+
+		/// <summary>
+		/// The start prefix for important log area.
+		/// Lines between start and end prefixes will be written even if <seealso cref="DetailedLogging"/> set to <c>false</c>.
+		/// </summary>
 		public const string LogImportantAreaStart = "// !<--";
+
+		/// <summary>
+		/// The end prefix for important log area.
+		/// Lines between start and end prefixes will be written even if <seealso cref="DetailedLogging"/> set to <c>false</c>.
+		/// </summary>
 		public const string LogImportantAreaEnd = "// !-->";
+		#endregion
 
 		#region Fields, .ctor & properties
 		private volatile int _importantAreaCount;
 
-		public HostLogger(ILogger wrappedLogger, bool detailedLogging)
+		/// <summary>Initializes a new instance of the <see cref="HostLogger"/> class.</summary>
+		/// <param name="wrappedLogger">The logger to redirect the output. Cannot be null.</param>
+		public HostLogger([NotNull] ILogger wrappedLogger) : this(wrappedLogger, false) { }
+
+		/// <summary>Initializes a new instance of the <see cref="HostLogger"/> class.</summary>
+		/// <param name="wrappedLogger">The logger to redirect the output. Cannot be null.</param>
+		/// <param name="detailedLogging">
+		/// Detailed logging mode.
+		/// If set to <c>false</c> only messages with LogImportant* prefixes wil be written.
+		/// </param>
+		public HostLogger([NotNull] ILogger wrappedLogger, bool detailedLogging)
 		{
-			if (wrappedLogger == null)
-				throw new ArgumentNullException(nameof(wrappedLogger));
+			Code.NotNull(wrappedLogger, nameof(wrappedLogger));
 
 			WrappedLogger = wrappedLogger;
 			DetailedLogging = detailedLogging;
 		}
 
+		/// <summary>The logger to redirect the output.</summary>
+		/// <value>TThe logger to redirect the output.</value>
 		protected ILogger WrappedLogger { get; }
+
+		/// <summary>
+		/// Detailed logging mode.
+		/// If set to <c>false</c> only messages with LogImportant* prefixes wil be written.
+		/// </summary>
+		/// <value><c>true</c> if detailed logging mode is enabled.</value>
 		public bool DetailedLogging { get; }
 		#endregion
 
+		/// <summary>Checks if the line should be written.</summary>
+		/// <param name="logKind">The kind of log message.</param>
+		/// <returns><c>true</c> if the line should be written.</returns>
 		// ReSharper disable once VirtualMemberNeverOverriden.Global
-		protected virtual bool ShouldWrite(LogKind kind) =>
-			_importantAreaCount > 0 ||
-				DetailedLogging ||
-				kind == LogKind.Error;
+		protected virtual bool ShouldWrite(LogKind logKind) =>
+			DetailedLogging ||
+				_importantAreaCount > 0 ||
+				logKind == LogKind.Error;
 
+		/// <summary>Handles the well-known prefixes for the line.</summary>
+		/// <param name="text">The text of the log line.</param>
+		/// <returns><c>true</c> if the line should be written.</returns>
 		// ReSharper disable once VirtualMemberNeverOverriden.Global
 		protected virtual bool PreprocessLine(string text)
 		{
+			if (DetailedLogging)
+				return true;
+
 			if (string.IsNullOrEmpty(text))
 				return false;
 
-			bool shouldWrite = false;
+			bool shouldWrite;
 #pragma warning disable 420
 			if (text.StartsWith(LogImportantAreaStart, StringComparison.Ordinal))
 			{
@@ -58,17 +110,24 @@ namespace CodeJam.PerfTests.Loggers
 			else if (text.StartsWith(LogImportantAreaEnd, StringComparison.Ordinal))
 			{
 				shouldWrite = true;
-				Interlocked.Decrement(ref _importantAreaCount);
+				// Decrements if value > 0
+				InterlockedOperations.Update(ref _importantAreaCount, i => Math.Max(i - 1, 0));
 			}
 #pragma warning restore 420
-			else if (text.StartsWith(LogImportantInfoPrefix, StringComparison.Ordinal))
+			else if (text.StartsWith(LogImportantInfoPrefix, StringComparison.Ordinal) ||
+				text.StartsWith(LogInfoPrefix, StringComparison.Ordinal))
 			{
 				shouldWrite = true;
+			}
+			else
+			{
+				shouldWrite = false;
 			}
 
 			return shouldWrite;
 		}
 
+		/// <summary>Writes the empty line.</summary>
 		public virtual void WriteLine()
 		{
 			if (ShouldWrite(LogKind.Default))
@@ -77,6 +136,9 @@ namespace CodeJam.PerfTests.Loggers
 			}
 		}
 
+		/// <summary>Writes the line.</summary>
+		/// <param name="logKind">Kind of text.</param>
+		/// <param name="text">The text to write.</param>
 		public virtual void WriteLine(LogKind logKind, string text)
 		{
 			// DONTTOUCH: the order of calls in condition is important.
@@ -86,6 +148,9 @@ namespace CodeJam.PerfTests.Loggers
 			}
 		}
 
+		/// <summary>Writes the test.</summary>
+		/// <param name="logKind">Kind of text.</param>
+		/// <param name="text">The text to write.</param>
 		public virtual void Write(LogKind logKind, string text)
 		{
 			if (ShouldWrite(logKind))
---
+++ PerfTests/tests/[L3_CompetitionsCore]/HostLoggerTests.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+using BenchmarkDotNet.Loggers;
+
+using CodeJam.PerfTests.Loggers;
+
+using NUnit.Framework;
+
+namespace CodeJam.PerfTests
+{
+	[TestFixture(Category = "BenchmarkDotNet")]
+	public class HostLoggerTests
+	{
+		private const string LogInput = @"
+not logged
+not logged
+
+// !logged
+// ! logged
+
+// ?logged
+// ? logged
+
+// not logged
+//! not logged
+//? not logged
+ // !not logged
+not logged// !not logged
+// !<-- logged
+logged
+logged
+
+logged
+// !--> logged
+// !--> logged
+// !--> logged
+// !--> logged
+// !--> logged
+
+not logged
+
+// !<-- logged
+logged
+// !logged
+// ! logged
+
+// ?logged
+// ? logged
+
+// logged
+//! logged
+//? logged
+
+logged
+// !--> logged
+
+not logged
+";
+
+		private const string ImportantLogOutput = @"// !logged
+// ! logged
+// ?logged
+// ? logged
+// !<-- logged
+logged
+logged
+
+logged
+// !--> logged
+// !--> logged
+// !--> logged
+// !--> logged
+// !--> logged
+// !<-- logged
+logged
+// !logged
+// ! logged
+
+// ?logged
+// ? logged
+
+// logged
+//! logged
+//? logged
+
+logged
+// !--> logged
+";
+
+		[Test]
+		public void TestHostLogger()
+		{
+			var lines = LogInput.Split(new[] { "\r\n" }, StringSplitOptions.None);
+			var output = new AccumulationLogger();
+			var logger = new HostLogger(output);
+			foreach (var line in lines)
+			{
+				logger.WriteLine(line);
+			}
+
+			Assert.AreEqual(output.GetLog(), ImportantLogOutput);
+
+			output = new AccumulationLogger();
+			logger = new HostLogger(output);
+
+			logger.Write(LogKind.Error, "A");
+			logger.Write(LogKind.Default, "B");
+			logger.Write(LogKind.Header, "C");
+			logger.Write(LogKind.Help, "D");
+			logger.Write(LogKind.Info, "E");
+			logger.Write(LogKind.Result, "F");
+			logger.Write(LogKind.Statistic, "G");
+			logger.Write(LogKind.Error, "H");
+			logger.WriteLine(LogKind.Error, "");
+			logger.WriteLine(LogKind.Error, "TST0");
+			logger.WriteLine(LogKind.Info, "// !TST1");
+			logger.WriteLine(LogKind.Info, "TST2");
+			logger.WriteLine(LogKind.Error, "// !<--");
+			logger.WriteLine(LogKind.Info, "TST3");
+			logger.WriteLine(LogKind.Info, "// !<--");
+			logger.WriteLine(LogKind.Info, "TST4");
+			logger.WriteLine();
+			logger.WriteLine(LogKind.Error, "TST5");
+			logger.WriteLine(LogKind.Default, "// !-->");
+			logger.WriteLine(LogKind.Default, "TST6");
+			logger.WriteLine(LogKind.Default, "// !-->");
+			logger.WriteLine(LogKind.Default, "TST7");
+			logger.WriteLine(LogKind.Default, @"TST8
+// !TST9
+TST10");
+
+			const string expected = @"AH
+TST0
+// !TST1
+// !<--
+TST3
+// !<--
+TST4
+
+TST5
+// !-->
+TST6
+// !-->
+";
+			Assert.AreEqual(output.GetLog(), expected);
+		}
+
+		[Test]
+		public void TestHostLoggerDetailedMode()
+		{
+			var lines = LogInput.Split(new[] { "\r\n" }, StringSplitOptions.None);
+			var output = new AccumulationLogger();
+			var logger = new HostLogger(output, true);
+			foreach (var line in lines)
+			{
+				logger.WriteLine(line);
+			}
+
+			Assert.AreEqual(output.GetLog(), LogInput + "\r\n");
+
+			output = new AccumulationLogger();
+			logger = new HostLogger(output, true);
+
+			logger.Write(LogKind.Error, "A");
+			logger.Write(LogKind.Default, "B");
+			logger.Write(LogKind.Header, "C");
+			logger.Write(LogKind.Help, "D");
+			logger.Write(LogKind.Info, "E");
+			logger.Write(LogKind.Result, "F");
+			logger.Write(LogKind.Statistic, "G");
+			logger.Write(LogKind.Error, "H");
+			logger.WriteLine(LogKind.Error, "");
+			logger.WriteLine(LogKind.Error, "TST0");
+			logger.WriteLine(LogKind.Info, "// !TST1");
+			logger.WriteLine(LogKind.Info, "TST2");
+			logger.WriteLine(LogKind.Error, "// !<--");
+			logger.WriteLine(LogKind.Info, "TST3");
+			logger.WriteLine(LogKind.Info, "// !<--");
+			logger.WriteLine(LogKind.Info, "TST4");
+			logger.WriteLine();
+			logger.WriteLine(LogKind.Error, "TST5");
+			logger.WriteLine(LogKind.Default, "// !-->");
+			logger.WriteLine(LogKind.Default, "TST6");
+			logger.WriteLine(LogKind.Default, "// !-->");
+			logger.WriteLine(LogKind.Default, "TST7");
+			logger.WriteLine(LogKind.Default, @"TST8
+// !TST9
+TST10");
+
+			const string expected = @"ABCDEFGH
+TST0
+// !TST1
+TST2
+// !<--
+TST3
+// !<--
+TST4
+
+TST5
+// !-->
+TST6
+// !-->
+TST7
+TST8
+// !TST9
+TST10
+";
+			Assert.AreEqual(output.GetLog(), expected);
+		}
+	}
+}
