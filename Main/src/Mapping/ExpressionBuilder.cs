b404265751d91ceff3ae1b4714f33f803c5be865
ig-sinicyn <igor.sinicyn@gmail.com>
2016-06-12 12:02:54 +0300

    Fix build

--- Main/src/Mapping/ExpressionBuilder.cs
+++ Main/src/Mapping/ExpressionBuilder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Runtime.InteropServices.WindowsRuntime;
 
 using CodeJam.Collections;
 
