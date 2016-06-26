238aeb65943b050e212d65c5e3a1a40e8cc33419
NN <nn1436401@gmail.com>
2016-06-26 18:32:50 +0300

    Add tests. Review changes.

---
+++ Main/src/Utilities/HGlobalScope`1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security;
+
+using JetBrains.Annotations;
+
+namespace CodeJam
+{
+	/// <summary>
+	/// Wraps Mashal.AllocHGlobal and Marshal.FreeHGlobal using generic.
+	/// </summary>
+	[PublicAPI]
+	[SecurityCritical]
+	public class HGlobalScope<T> : HGlobalScope where T : struct
+	{
+		/// <summary>
+		/// Default constructor, allocates memory with the size of <typeparamref name="T"/>
+		/// </summary>
+		[ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+		public HGlobalScope() : this(_size) { }
+
+		/// <summary>
+		/// Allocates memory from the unmanaged memory of the process by using the specified number of bytes.
+		/// </summary>
+		/// <param name="cb">The required number of bytes in memory.</param>
+		[ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+		public HGlobalScope(int cb) : base(checkSize(cb))
+		{
+		}
+		
+		/// <summary>
+		/// Value
+		/// </summary>
+		public T Value => (T)Marshal.PtrToStructure(Data, typeof(T));		
+
+		/// <summary>
+		/// Size of the of the generic parameter <typeparamref name="T"/>.
+		/// </summary>
+		private static readonly int _size = Marshal.SizeOf(typeof(T));
+
+		/// <summary>
+		/// Validate <paramref name="cb" /> is at least as the size of <typeparamref name="T"/>.
+		/// </summary>
+		/// <param name="cb">The required number of bytes in memory.</param>
+		/// <returns><paramref name="cb" /></returns>
+		private static int checkSize(int cb)
+		{
+			if (cb < _size)
+				throw new ArgumentException($"size is less than {_size}");
+
+			return cb;
+		}
+	}
+}
