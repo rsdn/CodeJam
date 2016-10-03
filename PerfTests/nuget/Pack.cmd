37b7fb6106f137b8419d6163d83fb02d350b3f98
ig-sinicyn <igor.sinicyn@gmail.com>
2016-10-03 14:06:09 +0300

    PerfTests - nuspec files

---
+++ PerfTests/nuget/Pack.cmd
@@ -0,0 +1,6 @@
+del *.nupkg
+
+NuGet Pack CodeJam.PerfTests.Core.nuspec
+NuGet Pack CodeJam.PerfTests.NUnit.nuspec
+NuGet Pack CodeJam.PerfTests.MSTest.nuspec
+NuGet Pack CodeJam.PerfTests.xUnit.nuspec
---
+++ PerfTests/nuget/Readme.txt
@@ -0,0 +1,4 @@
+CodeJam.PerfTests Release Notes
+-------------------------------
+
+TBD
