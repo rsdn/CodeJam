3dd725189fb710cc9f2b0d1449212f05f662df35
ig-sinicyn <igor.sinicyn@gmail.com>
2016-11-20 22:13:02 +0300

    Perftests build script: NuGet auto-push removed

--- PerfTests/nuget/Pack.cmd
+++ PerfTests/nuget/Pack.cmd
@@ -26,5 +26,3 @@ NuGet Pack CodeJam.PerfTests.Core.nuspec
 NuGet Pack CodeJam.PerfTests.NUnit.nuspec
 NuGet Pack CodeJam.PerfTests.MSTest.nuspec
 NuGet Pack CodeJam.PerfTests.xUnit.nuspec
-
-NuGet push *.nupkg
