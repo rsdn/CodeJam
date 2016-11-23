4345d4dfa3d082fd7c485573a2b5a5b486898391
andrewvk <avk@rsdn.org>
2016-11-23 22:15:00 +0300

    Comment unused methods

--- Main/src/Ranges/CompositeRangeInternal.cs
+++ Main/src/Ranges/CompositeRangeInternal.cs
@@ -37,39 +37,40 @@ namespace CodeJam.Ranges
 		#endregion
 
 		#region Helpers
-		/// <summary>Inserts the value into a sorted list.</summary>
-		/// <typeparam name="T">Type of the value.</typeparam>
-		/// <param name="sortedList">The sorted list.</param>
-		/// <param name="value">The value.</param>
-		/// <returns>Index of the inserted item.</returns>
-		internal static int InsertInSortedList<T>(
-			[NotNull] List<T> sortedList,
-			T value) =>
-				InsertInSortedList(sortedList, value, Comparer<T>.Default, false);
+		// Temporary comment unused internal methods
+		///// <summary>Inserts the value into a sorted list.</summary>
+		///// <typeparam name="T">Type of the value.</typeparam>
+		///// <param name="sortedList">The sorted list.</param>
+		///// <param name="value">The value.</param>
+		///// <returns>Index of the inserted item.</returns>
+		//internal static int InsertInSortedList<T>(
+		//	[NotNull] List<T> sortedList,
+		//	T value) =>
+		//		InsertInSortedList(sortedList, value, Comparer<T>.Default, false);
 
-		/// <summary>Inserts the value into a sorted list.</summary>
-		/// <typeparam name="T">Type of the value.</typeparam>
-		/// <param name="sortedList">The sorted list.</param>
-		/// <param name="value">The value.</param>
-		/// <param name="comparer">The comparer.</param>
-		/// <returns>Index of the inserted item.</returns>
-		internal static int InsertInSortedList<T>(
-			[NotNull] List<T> sortedList,
-			T value,
-			IComparer<T> comparer) =>
-				InsertInSortedList(sortedList, value, comparer, false);
+		///// <summary>Inserts the value into a sorted list.</summary>
+		///// <typeparam name="T">Type of the value.</typeparam>
+		///// <param name="sortedList">The sorted list.</param>
+		///// <param name="value">The value.</param>
+		///// <param name="comparer">The comparer.</param>
+		///// <returns>Index of the inserted item.</returns>
+		//internal static int InsertInSortedList<T>(
+		//	[NotNull] List<T> sortedList,
+		//	T value,
+		//	IComparer<T> comparer) =>
+		//		InsertInSortedList(sortedList, value, comparer, false);
 
-		/// <summary>Inserts the value into a sorted list.</summary>
-		/// <typeparam name="T">Type of the value.</typeparam>
-		/// <param name="sortedList">The sorted list.</param>
-		/// <param name="value">The value.</param>
-		/// <param name="skipDuplicates">If set to <c>true</c> duplicates are not inserted.</param>
-		/// <returns>Index of the inserted item (or existing one if <paramref name="skipDuplicates"/> is set to <c>true</c>).</returns>
-		internal static int InsertInSortedList<T>(
-			[NotNull] List<T> sortedList,
-			T value,
-			bool skipDuplicates) =>
-				InsertInSortedList(sortedList, value, null, skipDuplicates);
+		///// <summary>Inserts the value into a sorted list.</summary>
+		///// <typeparam name="T">Type of the value.</typeparam>
+		///// <param name="sortedList">The sorted list.</param>
+		///// <param name="value">The value.</param>
+		///// <param name="skipDuplicates">If set to <c>true</c> duplicates are not inserted.</param>
+		///// <returns>Index of the inserted item (or existing one if <paramref name="skipDuplicates"/> is set to <c>true</c>).</returns>
+		//internal static int InsertInSortedList<T>(
+		//	[NotNull] List<T> sortedList,
+		//	T value,
+		//	bool skipDuplicates) =>
+		//		InsertInSortedList(sortedList, value, null, skipDuplicates);
 
 		/// <summary>Inserts the value into a sorted list.</summary>
 		/// <typeparam name="T">Type of the value.</typeparam>
