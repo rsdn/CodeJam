77114ee5ff7f5bcfb01ab7ca3dd65fc2af443d7e
ig-sinicyn <igor.sinicyn@gmail.com>
2016-11-23 23:26:57 +0300

    Fix build

--- PerfTests/nuget/Pack.cmd
+++ PerfTests/nuget/Pack.cmd
@@ -1,4 +1,4 @@
-SET MSBUILD="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+SET MSBUILD="%ProgramFiles(x86)%\MSBuild\15.0\Bin\MSBuild.exe"
 
 cd ..\src
 %MSBUILD% CodeJam.PerfTests.csproj /target:Clean /property:Configuration=Release
