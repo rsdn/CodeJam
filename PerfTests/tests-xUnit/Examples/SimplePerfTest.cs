b80e503aff1b35bd387aab5f595d95af0f44806a
ig-sinicyn <igor.sinicyn@gmail.com>
2016-11-27 19:28:00 +0300

    Simple perf test: accuracy to +/-4% due to unstable appveyor timings proof: https://ci.appveyor.com/project/ig-sinicyn/codejam-dogfooding-perftests/build/0.0.22/tests

--- PerfTests/tests-xUnit/Examples/SimplePerfTest.cs
+++ PerfTests/tests-xUnit/Examples/SimplePerfTest.cs
@@ -20,20 +20,20 @@ namespace CodeJam.Examples
 		[CompetitionFact]
 		public void RunSimplePerfTest() => Competition.Run(this);
 
-		// Baseline competition member. Other competition members will be compared with this.
+		// Baseline competition member. Other competition members will be compared with this. Accuracy ±4%
 		[CompetitionBaseline]
 		public void Baseline() => Thread.SpinWait(Count);
 
 		// Competition member #1. Should take ~3x more time to run.
-		[CompetitionBenchmark(2.91, 3.09)]
+		[CompetitionBenchmark(2.88, 3.12)]
 		public void SlowerX3() => Thread.SpinWait(3 * Count);
 
 		// Competition member #2. Should take ~5x more time to run.
-		[CompetitionBenchmark(4.85, 5.15)]
+		[CompetitionBenchmark(4.80, 5.20)]
 		public void SlowerX5() => Thread.SpinWait(5 * Count);
 
 		// Competition member #3. Should take ~7x more time to run.
-		[CompetitionBenchmark(6.79, 7.21)]
+		[CompetitionBenchmark(6.72, 7.28)]
 		public void SlowerX7() => Thread.SpinWait(7 * Count);
 	}
 }
