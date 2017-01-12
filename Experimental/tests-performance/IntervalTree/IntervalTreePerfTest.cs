9913651e66bf35f529814e865acfb5a89b850b21
ig-sinicyn <igor.sinicyn@gmail.com>
2017-01-12 09:57:34 +0300

    CodeJam: interval tree experimental - perftests

--- Experimental/tests-performance/IntervalTree/IntervalTreePerfTest.cs
+++ Experimental/tests-performance/IntervalTree/IntervalTreePerfTest.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 using CodeJam.PerfTests;
-using CodeJam.Collections;
 using CodeJam.Strings;
+
+using IntervalTree;
 
 using NUnit.Framework;
 
@@ -13,51 +14,37 @@ namespace CodeJam.Ranges
 	[CompetitionBurstMode]
 	public class IntervalTreePerfTest
 	{
-		private const int Count = 10000;
+		private const int _count = 10000;
 
-		private readonly CompositeRange<int, string> ranges;
+		private readonly CompositeRange<int, string> _ranges;
 
-		private readonly IntervalTree<int, string> tree;
-		private readonly Range<int> intersection;
+		private readonly IntervalTree<int, string> _tree;
+		private readonly IntervalTreeCostin<int, string> _treeCostin;
+		private readonly Range<int> _intersection;
+		private readonly Interval<int> _intersectionCostin;
 
 		public IntervalTreePerfTest()
 		{
-			ranges = Enumerable.Range(0, Count)
-				.ToCompositeRange(i => 100 * i - 30 * (i % 11), i => 100 * i, i => i.ToString());
+			_ranges = Enumerable.Range(0, _count)
+				.ToCompositeRange(i => 4 * i - 2 * (i % 4), i => 1 + 4 * i + 2 * (i % 4), i => i.ToString());
+			;
 
-			intersection = ranges.SubRanges.TakeLast(100).First().WithoutKey();
+			_intersection = _ranges.SubRanges[1000].WithoutKey();
+			_intersectionCostin = new Interval<int>(_intersection.FromValue, _intersection.ToValue);
 
-			tree = new IntervalTree<int, string>(ranges);
-		}
-
-
-		[Test]
-		public void TestEqual()
-		{
-			Assert.AreEqual(
-				ranges.SubRanges.Where(r => r.HasIntersection(intersection)).ToArray(),
-				IntersectNaive(intersection).ToArray());
-
-			Assert.AreEqual(
-				tree.Intersect(intersection).Join(";"),
-				IntersectNaive(intersection).ToArray().Join(";"));
-		}
-
-		//[Test]
-		//public void Test() => Competition.Run(this);
-
-		[CompetitionBaseline]
-		public void Intersect()
-		{
-			IntersectNaive(intersection);
+			_tree = new IntervalTree<int, string>(_ranges);
+			_treeCostin = new IntervalTreeCostin<int, string>(
+				_ranges.SubRanges.Select(r => new KeyValuePair<IInterval<int>, string>(
+					new Interval<int>(r.FromValue, r.ToValue), r.Key))
+				);
 		}
 
 		private List<Range<int, string>> IntersectNaive(Range<int> check)
 		{
 			var result = new List<Range<int, string>>();
-			if (!ranges.ContainingRange.HasIntersection(check))
+			if (!_ranges.ContainingRange.HasIntersection(check))
 				return result;
-			foreach (var range in ranges.SubRanges)
+			foreach (var range in _ranges.SubRanges)
 			{
 				if (range.From > check.To)
 					break;
@@ -67,7 +54,35 @@ namespace CodeJam.Ranges
 			return result;
 		}
 
-		[CompetitionBenchmark(0.0154, 0.0182)]
-		public void IntersectTree() => tree.Intersect(intersection);
+		[Test]
+		public void TestIntervalTreeEqual()
+		{
+			Assert.AreEqual(
+				_ranges.SubRanges.Where(r => r.HasIntersection(_intersection)).ToArray(),
+				IntersectNaive(_intersection).ToArray());
+
+			Assert.AreEqual(
+				_tree.Intersect(_intersection).Join(";"),
+				IntersectNaive(_intersection).ToArray().Join(";"));
+
+			Assert.AreEqual(
+				_treeCostin
+					.GetIntervalsOverlappingWith(_intersectionCostin)
+					.Select(i => new Range<int, string>(i.Key.Start, i.Key.End, i.Value))
+					.Join(";"),
+				IntersectNaive(_intersection).ToArray().Join(";"));
+		}
+
+		[Test]
+		public void RunIntervalTreePerfTest() => Competition.Run(this);
+
+		[CompetitionBaseline]
+		public void Intersect() => IntersectNaive(_intersection);
+
+		[CompetitionBenchmark(0.083, 0.092)]
+		public void IntersectTree() => _tree.Intersect(_intersection);
+
+		[CompetitionBenchmark(0.21, 0.27)]
+		public void IntersectCostin() => _treeCostin.GetIntervalsOverlappingWith(_intersectionCostin).ToArray();
 	}
 }
