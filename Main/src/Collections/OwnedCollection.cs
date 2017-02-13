07f6c251da55ccb1ce576adede105683d6b8f609
ig-sinicyn <igor.sinicyn@gmail.com>
2017-02-13 17:25:59 +0300

    CodeJam: MappedCollections: needs review.

---
+++ Main/src/Collections/OwnedCollection.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace CodeJam.Collections
+{
+	// DONTTOUCH: please, DO NOT move the OwnedCollection<TKey, TItem, TOwner> into its own file.
+	// The code in it is a copy of the OwnedCollection<TItem, TOwner> and it's easier 
+	// to keep them in sync when both are in single file.
+
+	/// <summary>Base collection type that allows to associate collection itens with the owner.</summary>
+	/// <typeparam name="TItem">The type of the item.</typeparam>
+	/// <typeparam name="TOwner">The type of the owner.</typeparam>
+	/// <seealso cref="System.Collections.ObjectModel.Collection{TItem}"/>
+	public abstract class OwnedCollection<TItem, TOwner> : Collection<TItem>
+		where TItem : class
+		where TOwner : class
+	{
+		private readonly TOwner _owner;
+		private readonly Func<TItem, TOwner> _ownerGetter;
+		private readonly Action<TItem, TOwner> _ownerSetter;
+
+		/// <summary>Initializes a new instance of the <see cref="OwnedCollection{TItem, TOwner}"/> class.</summary>
+		/// <param name="owner">The owner for the collection.</param>
+		/// <param name="ownerGetter">Item getter for the owner.</param>
+		/// <param name="ownerSetter">Item setter for the owner.</param>
+		protected OwnedCollection(
+			TOwner owner,
+			Func<TItem, TOwner> ownerGetter,
+			Action<TItem, TOwner> ownerSetter)
+		{
+			Code.NotNull(owner, nameof(owner));
+			Code.NotNull(ownerGetter, nameof(ownerGetter));
+			Code.NotNull(ownerSetter, nameof(ownerSetter));
+
+			_owner = owner;
+			_ownerGetter = ownerGetter;
+			_ownerSetter = ownerSetter;
+		}
+
+		#region Copy this into OwnedCollection<TKey, TItem, TOwner>
+		/// <summary>
+		/// Removes all elements from the <see cref="T:System.Collections.ObjectModel.Collection`1"/>.
+		/// Clears owner for the items being removed.
+		/// </summary>
+		protected override void ClearItems()
+		{
+			foreach (var item in Items)
+			{
+				Code.BugIf(item == null, "Bug: one of items in collection is null.");
+				_ownerSetter(item, null);
+			}
+			base.ClearItems();
+		}
+
+		/// <summary>
+		/// Inserts an element into the <see cref="T:System.Collections.ObjectModel.Collection`1"/> at the specified index.
+		/// Sets owner for the items being added.
+		/// </summary>
+		/// <param name="index">The zero-based index at which <paramref name="item"/> should be inserted.</param>
+		/// <param name="item">The object to insert. The value can be null for reference types.</param>
+		protected override void InsertItem(int index, TItem item)
+		{
+			Code.NotNull(item, nameof(item));
+
+			Code.AssertState(_ownerGetter(item) == null, "Cannot add an item as it is mapped to another owner.");
+			base.InsertItem(index, item);
+			_ownerSetter(item, _owner);
+		}
+
+		/// <summary>
+		/// Removes the element at the specified index of the <see cref="T:System.Collections.ObjectModel.Collection`1"/>.
+		/// Clears owner for the item being removed.
+		/// </summary>
+		/// <param name="index">The zero-based index of the element to remove.</param>
+		protected override void RemoveItem(int index)
+		{
+			var item = this[index];
+			Code.BugIf(item == null, "Bug: one of items in collection is null.");
+
+			base.RemoveItem(index);
+			_ownerSetter(item, null);
+		}
+
+		/// <summary>
+		/// Replaces the element at the specified index.
+		/// Sets owner for the items being added.
+		/// Clears owner for the item being removed.
+		/// </summary>
+		/// <param name="index">The zero-based index of the element to replace.</param>
+		/// <param name="item">
+		/// The new value for the element at the specified index. The value can be null for reference types.
+		/// </param>
+		protected override void SetItem(int index, TItem item)
+		{
+			Code.NotNull(item, nameof(item));
+			Code.AssertState(_ownerGetter(item) == null, "Cannot add an item as it is mapped to another owner.");
+
+			var oldItem = this[index];
+			Code.BugIf(oldItem == null, "Bug: one of items in collection is null.");
+			_ownerSetter(oldItem, null);
+			base.SetItem(index, item);
+			_ownerSetter(item, _owner);
+		}
+		#endregion
+	}
+
+	/// <summary>Base keyed collection type that allows to associate collection itens with the owner.</summary>
+	/// <typeparam name="TKey">The type of the key.</typeparam>
+	/// <typeparam name="TItem">The type of the item.</typeparam>
+	/// <typeparam name="TOwner">The type of the owner.</typeparam>
+	/// <seealso cref="System.Collections.ObjectModel.Collection{TItem}"/>
+	public abstract class OwnedCollection<TKey, TItem, TOwner> : KeyedCollection<TKey, TItem>
+		where TItem : class
+		where TOwner : class
+	{
+		private readonly TOwner _owner;
+		private readonly Func<TItem, TKey> _keyGetter;
+		private readonly Func<TItem, TOwner> _ownerGetter;
+		private readonly Action<TItem, TOwner> _ownerSetter;
+
+		/// <summary>Initializes a new instance of the <see cref="OwnedCollection{TItem, TOwner}"/> class.</summary>
+		/// <param name="owner">The owner for the collection.</param>
+		/// <param name="keyGetter">The get key.</param>
+		/// <param name="ownerGetter">Item getter for the owner.</param>
+		/// <param name="ownerSetter">Item setter for the owner.</param>
+		protected OwnedCollection(
+			TOwner owner,
+			Func<TItem, TKey> keyGetter,
+			Func<TItem, TOwner> ownerGetter,
+			Action<TItem, TOwner> ownerSetter)
+		{
+			Code.NotNull(owner, nameof(owner));
+			Code.NotNull(keyGetter, nameof(keyGetter));
+			Code.NotNull(ownerGetter, nameof(ownerGetter));
+			Code.NotNull(ownerSetter, nameof(ownerSetter));
+
+			_owner = owner;
+			_keyGetter = keyGetter;
+			_ownerGetter = ownerGetter;
+			_ownerSetter = ownerSetter;
+		}
+
+		/// <summary>
+		/// Получение ключа для элемента
+		/// </summary>
+		protected override TKey GetKeyForItem(TItem item)
+		{
+			Code.NotNull(item, nameof(item));
+
+			var result = _keyGetter(item);
+			Code.AssertState(result != null, "The key of the item should be not null.");
+			return result;
+		}
+
+		#region Copied from OwnedCollection<TKey, TItem, TOwner>
+		/// <summary>
+		/// Removes all elements from the <see cref="T:System.Collections.ObjectModel.Collection`1"/>.
+		/// Clears owner for the items being removed.
+		/// </summary>
+		protected override void ClearItems()
+		{
+			foreach (var item in Items)
+			{
+				Code.BugIf(item == null, "Bug: one of items in collection is null.");
+				_ownerSetter(item, null);
+			}
+			base.ClearItems();
+		}
+
+		/// <summary>
+		/// Inserts an element into the <see cref="T:System.Collections.ObjectModel.Collection`1"/> at the specified index.
+		/// Sets owner for the items being added.
+		/// </summary>
+		/// <param name="index">The zero-based index at which <paramref name="item"/> should be inserted.</param>
+		/// <param name="item">The object to insert. The value can be null for reference types.</param>
+		protected override void InsertItem(int index, TItem item)
+		{
+			Code.NotNull(item, nameof(item));
+
+			Code.AssertState(_ownerGetter(item) == null, "Cannot add an item as it is mapped to another owner.");
+			base.InsertItem(index, item);
+			_ownerSetter(item, _owner);
+		}
+
+		/// <summary>
+		/// Removes the element at the specified index of the <see cref="T:System.Collections.ObjectModel.Collection`1"/>.
+		/// Clears owner for the item being removed.
+		/// </summary>
+		/// <param name="index">The zero-based index of the element to remove.</param>
+		protected override void RemoveItem(int index)
+		{
+			var item = this[index];
+			Code.BugIf(item == null, "Bug: one of items in collection is null.");
+
+			base.RemoveItem(index);
+			_ownerSetter(item, null);
+		}
+
+		/// <summary>
+		/// Replaces the element at the specified index.
+		/// Sets owner for the items being added.
+		/// Clears owner for the item being removed.
+		/// </summary>
+		/// <param name="index">The zero-based index of the element to replace.</param>
+		/// <param name="item">
+		/// The new value for the element at the specified index. The value can be null for reference types.
+		/// </param>
+		protected override void SetItem(int index, TItem item)
+		{
+			Code.NotNull(item, nameof(item));
+			Code.AssertState(_ownerGetter(item) == null, "Cannot add an item as it is mapped to another owner.");
+
+			_ownerSetter(this[index], null);
+			base.SetItem(index, item);
+			_ownerSetter(item, _owner);
+		}
+		#endregion
+	}
+}
---
+++ Main/tests/Collections/OwnedCollectionTests.cs
@@ -0,0 +1,244 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using static NUnit.Framework.Assert;
+
+namespace CodeJam.Collections
+{
+	public static class OwnedCollectionTests
+	{
+		public class Owner
+		{
+			public Owner()
+			{
+				Items = new MyCollection(this);
+				KeyedItems = new MyKeyedCollection(this);
+			}
+
+			public MyCollection Items { get; }
+			public MyKeyedCollection KeyedItems { get; }
+		}
+
+		public class Item
+		{
+			public Guid Key { get; } = Guid.NewGuid();
+			public Owner Owner { get; set; }
+		}
+
+		public class MyCollection : OwnedCollection<Item, Owner>
+		{
+			public MyCollection(Owner owner)
+				: base(owner, i => i.Owner, (i, o) => i.Owner = o) { }
+		}
+
+		public class MyKeyedCollection : OwnedCollection<Guid, Item, Owner>
+		{
+			public MyKeyedCollection(Owner owner)
+				: base(owner, i => i.Key, i => i.Owner, (i, o) => i.Owner = o) { }
+		}
+
+		[Test]
+		public static void TestOwnedAdd()
+		{
+			var owner1 = new Owner();
+			var owner2 = new Owner();
+
+			var item1 = new Item();
+			var item2 = new Item();
+
+			// Success
+			// Add item 1
+			owner1.Items.Add(item1);
+			AreEqual(item1.Owner, owner1);
+			AreEqual(item2.Owner, null);
+
+			AreEqual(owner1.Items.Count, 1);
+			AreEqual(owner1.Items[0], item1);
+			IsTrue(owner1.Items.Contains(item1));
+
+			AreEqual(owner1.KeyedItems.Count, 0);
+			IsFalse(owner1.KeyedItems.Contains(item2));
+			IsFalse(owner1.KeyedItems.Contains(item2.Key));
+
+			// Add item 2
+			owner1.KeyedItems.Add(item2);
+			AreEqual(item1.Owner, owner1);
+			AreEqual(item2.Owner, owner1);
+
+			AreEqual(owner1.Items.Count, 1);
+			AreEqual(owner1.Items[0], item1);
+			IsTrue(owner1.Items.Contains(item1));
+
+			AreEqual(owner1.KeyedItems.Count, 1);
+			AreEqual(owner1.KeyedItems[0], item2);
+			AreEqual(owner1.KeyedItems[item2.Key], item2);
+
+			// Failures
+			Throws<ArgumentNullException>(() => owner1.Items.Add(null));
+			Throws<ArgumentNullException>(() => owner1.KeyedItems.Add(null));
+			Throws<InvalidOperationException>(() => owner1.Items.Add(item1));
+			Throws<InvalidOperationException>(() => owner1.Items.Add(item2));
+			Throws<InvalidOperationException>(() => owner1.KeyedItems.Add(item1));
+			Throws<InvalidOperationException>(() => owner1.KeyedItems.Add(item2));
+			Throws<InvalidOperationException>(() => owner2.Items.Add(item1));
+			Throws<InvalidOperationException>(() => owner2.Items.Add(item2));
+			Throws<InvalidOperationException>(() => owner2.KeyedItems.Add(item1));
+			Throws<InvalidOperationException>(() => owner2.KeyedItems.Add(item2));
+		}
+
+		[Test]
+		public static void TestOwnedInsert()
+		{
+			var owner1 = new Owner()
+			{
+				Items =
+				{
+					new Item(),
+					new Item(),
+					new Item()
+				},
+				KeyedItems =
+				{
+					new Item()
+				}
+			};
+
+			var item1 = new Item();
+			var item2 = new Item();
+
+			// Success
+			// Insert item 1
+			owner1.Items.Insert(0, item1);
+			AreEqual(item1.Owner, owner1);
+			AreEqual(item2.Owner, null);
+
+			AreEqual(owner1.Items.Count, 4);
+			AreEqual(owner1.Items[0], item1);
+			AreEqual(owner1.Items.IndexOf(item1), 0);
+
+			AreEqual(owner1.KeyedItems.Count, 1);
+			AreEqual(owner1.KeyedItems.IndexOf(item2), -1);
+			IsFalse(owner1.KeyedItems.Contains(item2.Key));
+
+			// Insert item 2
+			owner1.KeyedItems.Insert(1, item2);
+			AreEqual(item1.Owner, owner1);
+			AreEqual(item2.Owner, owner1);
+
+			AreEqual(owner1.Items.Count, 4);
+			AreEqual(owner1.Items[0], item1);
+			AreEqual(owner1.Items.IndexOf(item1), 0);
+
+			AreEqual(owner1.KeyedItems.Count, 2);
+			AreEqual(owner1.KeyedItems.IndexOf(item2), 1);
+			AreEqual(owner1.KeyedItems[1], item2);
+			AreEqual(owner1.KeyedItems[item2.Key], item2);
+
+			// Failures
+			Throws<ArgumentNullException>(() => owner1.Items.Insert(0, null));
+			Throws<ArgumentNullException>(() => owner1.KeyedItems.Insert(0, null));
+			Throws<InvalidOperationException>(() => owner1.Items.Insert(0, item1));
+			Throws<InvalidOperationException>(() => owner1.Items.Insert(0, item2));
+			Throws<InvalidOperationException>(() => owner1.KeyedItems.Insert(0, item1));
+			Throws<InvalidOperationException>(() => owner1.KeyedItems.Insert(0, item2));
+		}
+
+		[Test]
+		public static void TestOwnedSet()
+		{
+			var owner = new Owner();
+
+			var item1 = new Item();
+			var item2 = new Item();
+
+			// Success
+			// Add item 1
+			owner.Items.Add(item1);
+			AreEqual(item1.Owner, owner);
+			AreEqual(item2.Owner, null);
+			AreEqual(owner.Items[0], item1);
+
+			// Set item 2
+			owner.Items[0] = item2;
+			AreEqual(item1.Owner, null);
+			AreEqual(item2.Owner, owner);
+			AreEqual(owner.Items[0], item2);
+
+			DoesNotThrow(() => owner.KeyedItems.Add(item1));
+			Throws<InvalidOperationException>(() => owner.Items[0] = owner.Items[0]);
+			Throws<InvalidOperationException>(() => owner.KeyedItems[0] = owner.Items[0]);
+
+			AreEqual(item1.Owner, owner);
+			AreEqual(item2.Owner, owner);
+			AreEqual(owner.Items[0], item2);
+			AreEqual(owner.KeyedItems[0], item1);
+		}
+
+		[Test]
+		public static void TestOwnedClear()
+		{
+			var owner = new Owner();
+
+			var item1 = new Item();
+			var item2 = new Item();
+
+			owner.Items.Add(item1);
+			owner.KeyedItems.Add(item2);
+			AreEqual(item1.Owner, owner);
+			AreEqual(item2.Owner, owner);
+			AreEqual(owner.Items[0], item1);
+			AreEqual(owner.KeyedItems[0], item2);
+			Throws<InvalidOperationException>(() => owner.Items.Add(item1));
+			Throws<InvalidOperationException>(() => owner.KeyedItems.Add(item2));
+
+			owner.Items.Clear();
+			AreEqual(item1.Owner, null);
+			AreEqual(item2.Owner, owner);
+			AreEqual(owner.Items.Count, 0);
+			AreEqual(owner.KeyedItems.Count, 1);
+
+			owner.KeyedItems.Clear();
+			AreEqual(item1.Owner, null);
+			AreEqual(item2.Owner, null);
+			AreEqual(owner.Items.Count, 0);
+			AreEqual(owner.KeyedItems.Count, 0);
+
+			DoesNotThrow(() => owner.Items.Add(item1));
+			DoesNotThrow(() => owner.KeyedItems.Add(item2));
+		}
+
+		[Test]
+		public static void TestOwnedRemove()
+		{
+			var owner = new Owner();
+
+			var item1 = new Item();
+			var item2 = new Item();
+
+			owner.Items.Add(item1);
+			owner.KeyedItems.Add(item2);
+			AreEqual(item1.Owner, owner);
+			AreEqual(item2.Owner, owner);
+			AreEqual(owner.Items[0], item1);
+			AreEqual(owner.KeyedItems[0], item2);
+			Throws<InvalidOperationException>(() => owner.Items.Add(item1));
+			Throws<InvalidOperationException>(() => owner.KeyedItems.Add(item2));
+
+			owner.Items.RemoveAt(0);
+			AreEqual(item1.Owner, null);
+			AreEqual(item2.Owner, owner);
+			AreEqual(owner.Items.Count, 0);
+			AreEqual(owner.KeyedItems.Count, 1);
+
+			owner.KeyedItems.Remove(item2.Key);
+			AreEqual(item1.Owner, null);
+			AreEqual(item2.Owner, null);
+			AreEqual(owner.Items.Count, 0);
+			AreEqual(owner.KeyedItems.Count, 0);
+
+			DoesNotThrow(() => owner.Items.Add(item1));
+			DoesNotThrow(() => owner.KeyedItems.Add(item2));
+		}
+	}
+}
