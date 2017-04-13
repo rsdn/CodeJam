999b56c5e3bc9be7b6f037c7bd7c4d882f46b09c
ig-sinicyn <igor.sinicyn@gmail.com>
2017-04-13 07:57:56 +0300

    fix build

--- Main/src/Compile.cmd
+++ Main/src/Compile.cmd
@@ -1,5 +1,8 @@
+dotnet restore CodeJam.Main.csproj
+
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" CodeJam.Main.csproj /target:Clean   /property:Configuration=Debug
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" CodeJam.Main.csproj /target:Clean   /property:Configuration=Release
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" CodeJam.Main.csproj /target:ReBuild /property:Configuration=Debug
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" CodeJam.Main.csproj /target:ReBuild /property:Configuration=Release
+
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" CodeJam.Main.csproj /target:Pack    /property:Configuration=Release
