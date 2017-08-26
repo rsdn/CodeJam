74bffed26d7743214a2d98b8dff02eafd6bbf876
rameel <rameel-b@hotmail.com>
2017-08-26 20:58:44 +0600

    TaskHelper: additional overloads

---
+++ Main/src/Threading/TaskHelper.Marker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Threading
+{
+	/// <summary>
+	/// Helper methods for <see cref="Task"/> and <see cref="Task{TResult}"/>.
+	/// </summary>
+	[PublicAPI]
+	public static partial class TaskHelper
+	{
+	}
+}
