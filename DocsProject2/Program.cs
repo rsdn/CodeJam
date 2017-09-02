83765e1b57158ebe1018a838b097195734c37f2b
Andrew Koryavchenko <avk@rsdn.org>
2017-09-02 16:42:45 +0300

    Working on docs

---
+++ DocsProject2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DocsProject2
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost
+				.CreateDefaultBuilder(args)
+				.UseKestrel()
+				.UseContentRoot("s:/work/codejam/docs")
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.Build();
+	}
+}
