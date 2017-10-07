a4715fc22d6eb026c116d1b7672ba3927c677548
ig-sinicyn <igor.sinicyn@gmail.com>
2017-10-07 12:16:52 +0300

    Appveyor: test result uploading (troubleshooting nunit 3 reports)

--- Build/CodeJam.AppVeyor.NUnit.Tests.ps1
+++ Build/CodeJam.AppVeyor.NUnit.Tests.ps1
@@ -1,13 +1,21 @@
 $include = "*-tests.dll", "*-tests.performance.dll", "*-tests.NUnit.dll"
 $exclude = "Experimental\\.*?\\CodeJam-Tests.Performance.dll"
 
+$wc = New-Object System.Net.WebClient
+
 #run .net tests
+$logFileName = "$env:APPVEYOR_BUILD_FOLDER\_Results\net_nunit_results.xml"
 $a = (gci -include $include -r | `
 	where { $_.fullname -match "\\bin\\Publish\\net\d" -and $_.fullname -notmatch $exclude } | `
 	select -ExpandProperty FullName)
-echo "nunit3-console $a --result=myresults.xml;format=AppVeyor"
-&"nunit3-console" $a "--result=myresults.xml;format=AppVeyor"
+echo "nunit3-console $a --result=$logFileName"
+&"nunit3-console" $a "--result=$logFileName"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID", "$logFileName")
+if ($LastExitCode -ne 0) { 
+	echo "FAIL: UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $logFileName"
+	$host.SetShouldExit($LastExitCode)
+}
 
 #run .net core tests
 $a = (gci -include $include -r | `
@@ -17,7 +25,12 @@ $a = (gci -include $include -r | `
 $logFileName = "$env:APPVEYOR_BUILD_FOLDER\_Results\netcore_nunit_results.xml"
 echo "dotnet vstest $a --logger:'trx;LogFileName=$logFileName'"
 dotnet vstest $a --logger:"trx;LogFileName=$logFileName"
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
-$wc = New-Object System.Net.WebClient
+if ($LastExitCode -ne 0) {
+	echo "FAIL: dotnet vstest $a --logger:'trx;LogFileName=$logFileName'"
+	$host.SetShouldExit($LastExitCode)
+}
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID", "$logFileName")
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) { 
+	echo "FAIL: UploadFile: https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID from $logFileName"
+	$host.SetShouldExit($LastExitCode)
+}
