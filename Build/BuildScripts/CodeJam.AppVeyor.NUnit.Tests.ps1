330d170dbac13dff5d1887a04776772b7426b730
NN <nn1436401@gmail.com>
2019-02-04 15:15:30 +0200

    Fix test run

--- Build/BuildScripts/CodeJam.AppVeyor.NUnit.Tests.ps1
+++ Build/BuildScripts/CodeJam.AppVeyor.NUnit.Tests.ps1
@@ -13,9 +13,8 @@ foreach ($target in $targetsDotNet) {
 	$a = (gci -include $include -r | `
 		where { $_.fullname -match "\\bin\\Release\\$($target)" -and $_.fullname -notmatch $exclude } | `
 		select -ExpandProperty FullName)
-	$cmdRunTest = "nunit3-console $a --result=$logFileName"
-	echo $cmdRunTest
-	$cmdRunTest
+	echo "nunit3-console $a --result=$logFileName"
+	&"nunit3-console" $a "--result=$logFileName"
 	if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 	echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $logFileName"
 	$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID", "$logFileName")
@@ -25,7 +24,7 @@ foreach ($target in $targetsDotNet) {
 	}
 }
 
-$targetsDotNetWithRunner = "net45","net40","net35","net20"
+$targetsDotNetWithRunner = "net45","net40","net35","net30","net20"
 
 foreach ($target in $targetsDotNetWithRunner) {
 	#run .net tests
@@ -34,9 +33,8 @@ foreach ($target in $targetsDotNetWithRunner) {
 		where { $_.fullname -match "\\bin\\Release\\$($target)" -and $_.fullname -notmatch $exclude } | `
 		select -ExpandProperty FullName)
 	$framework = $target.Substring($target.Length - 2, 1) + "." + $target.Substring($target.Length - 1)
-	$cmdRunTest = "nunit3-console $a --framework=$($framework) --result=$logFileName"
-	echo $cmdRunTest
-	&$cmdRunTest
+	echo "nunit3-console $a --framework=$($framework) --result=$logFileName"
+	&"nunit3-console" $a "--framework=$($framework) --result=$logFileName"
 	if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 	echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $logFileName"
 	$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID", "$logFileName")
