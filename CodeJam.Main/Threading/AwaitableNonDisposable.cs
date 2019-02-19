c0ae7cc7582a441dbdf2e89680e561daa44f1886
NN <nn1436401@gmail.com>
2019-02-19 11:25:33 +0200

    Fix doc of #82

--- CodeJam.Main/Threading/AwaitableNonDisposable.cs
+++ CodeJam.Main/Threading/AwaitableNonDisposable.cs
@@ -8,62 +8,68 @@ using JetBrains.Annotations;
 
 namespace CodeJam.Threading
 {
-    /// <summary>
-    /// An awaitable wrapper around a task whose result is <see cref="IDisposable"/>.
+	/// <summary>
+	/// An awaitable wrapper around a task whose result is <see cref="IDisposable"/>.
 	/// The wrapper itself is not <see cref="IDisposable"/>.!--
 	/// This prevents usage errors like <code>using (lock.AcquireAsync())</code> when the appropriate usage should be <code>using (await lock.AcquireAsync())</code>.
-    /// </summary>
-    /// <typeparam name="T">The type of the result of the underlying task.</typeparam>
-    public struct AwaitableNonDisposable<T> where T : IDisposable
-    {
-        /// <summary>
-        /// The underlying task.
-        /// </summary>
-        [NotNull] private readonly Task<T> _task;
+	/// </summary>
+	/// <typeparam name="T">The type of the result of the underlying task.</typeparam>
+	public struct AwaitableNonDisposable<T> where T : IDisposable
+	{
+		/// <summary>
+		/// The underlying task.
+		/// </summary>
+		[NotNull] private readonly Task<T> _task;
 
-        /// <summary>
-        /// Initializes a new awaitable wrapper around the specified task.
-        /// </summary>
-        /// <param name="task">The underlying task to wrap. This may not be <c>null</c>.</param>
-        public AwaitableNonDisposable([NotNull] Task<T> task)
-        {
+		/// <summary>
+		/// Initializes a new awaitable wrapper around the specified task.
+		/// </summary>
+		/// <param name="task">The underlying task to wrap. This may not be <c>null</c>.</param>
+		public AwaitableNonDisposable([NotNull] Task<T> task)
+		{
 			Code.NotNull(task, nameof(task));
 
-            _task = task;
-        }
+			_task = task;
+		}
 
-        /// <summary>
-        /// Returns the underlying task.
-        /// </summary>
-        public Task<T> AsTask()
-        {
-            return _task;
-        }
+		/// <summary>
+		/// Returns the underlying task.
+		/// </summary>
+		/// <returns>Underlying task.</returns>
+		[NotNull]
+		public Task<T> AsTask()
+		{
+			return _task;
+		}
 
-        /// <summary>
-        /// Implicit conversion to the underlying task.
-        /// </summary>
-        /// <param name="source">The awaitable wrapper.</param>
-        public static implicit operator Task<T>([NotNull] AwaitableNonDisposable<T> source)
-        {
-            return source.AsTask();
-        }
+		/// <summary>
+		/// Implicit conversion to the underlying task.
+		/// </summary>
+		/// <param name="source">The awaitable wrapper.</param>
+		/// <returns>Underlying task</returns>
+		[NotNull]
+		public static implicit operator Task<T>([NotNull] AwaitableNonDisposable<T> source)
+		{
+			return source.AsTask();
+		}
 
-        /// <summary>
-        /// Infrastructure. Returns the task awaiter for the underlying task.
-        /// </summary>
-        public TaskAwaiter<T> GetAwaiter()
-        {
-            return _task.GetAwaiter();
-        }
+		/// <summary>
+		/// Infrastructure. Returns the task awaiter for the underlying task.
+		/// </summary>
+		/// <returns>Task awaiter for the underlying task.</returns>
+		public TaskAwaiter<T> GetAwaiter()
+		{
+			return _task.GetAwaiter();
+		}
 
-        /// <summary>
-        /// Infrastructure. Returns a configured task awaiter for the underlying task.
-        /// </summary>
-        /// <param name="continueOnCapturedContext">Whether to attempt to marshal the continuation back to the captured context.</param>
-        public ConfiguredTaskAwaitable<T> ConfigureAwait(bool continueOnCapturedContext)
-        {
-            return _task.ConfigureAwait(continueOnCapturedContext);
-        }
-    }
+		/// <summary>
+		/// Infrastructure. Returns a configured task awaiter for the underlying task.
+		/// </summary>
+		/// <param name="continueOnCapturedContext">Whether to attempt to marshal the continuation back to the captured context.</param>
+		/// <returns>A configured task awaiter for the underlying task.</returns>
+		public ConfiguredTaskAwaitable<T> ConfigureAwait(bool continueOnCapturedContext)
+		{
+			return _task.ConfigureAwait(continueOnCapturedContext);
+		}
+	}
 }
