e62e2e95d4c791b041d680d85200f557950580fc
ig-sinicyn <igor.sinicyn@gmail.com>
2019-12-14 02:00:03 +0300

    + TimeSpanHelpers

---
+++ CodeJam.Main.Tests/Dates/TimeSpanHelperTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace CodeJam.Dates
+{
+	public static class TimeSpanHelperTests
+	{
+		[TestCase(1, 2)]
+		[TestCase(1, 0)]
+		[TestCase(1, -1)]
+		[TestCase(100, 0.5)]
+		[TestCase(100, -0.25)]
+		[TestCase(1, 0.125)]
+		[TestCase(123, 456.25)]
+		public static void TestMultiplySuccess(int time, double scale)
+		{
+			// Too small for timespan resolution
+			Assert.AreEqual(
+				TimeSpan.FromTicks(time * 10000).Multiply(scale),
+				TimeSpan.FromTicks((long)(time * 10000 * scale)));
+
+			// Too small for timespan resolution
+			Assert.AreEqual(
+				TimeSpanHelpers.FromNanoseconds(time * 10000).Multiply(scale),
+				TimeSpanHelpers.FromNanoseconds(time * 10000 * scale));
+
+			// Too small for timespan resolution
+			Assert.AreEqual(
+				TimeSpanHelpers.FromMicroseconds(time * 10000).Multiply(scale),
+				TimeSpanHelpers.FromMicroseconds(time * 10000 * scale));
+
+			// HACK: see https://stackoverflow.com/a/5450495 for explanation
+			Assert.AreEqual(
+				Math.Round(TimeSpan.FromMilliseconds(time).Multiply(scale).TotalMilliseconds),
+				Math.Round(TimeSpan.FromMilliseconds(time * scale).TotalMilliseconds));
+
+			Assert.AreEqual(
+				TimeSpan.FromSeconds(time).Multiply(scale),
+				TimeSpan.FromSeconds(time * scale));
+
+			Assert.AreEqual(
+				TimeSpan.FromMinutes(time).Multiply(scale),
+				TimeSpan.FromMinutes(time * scale));
+
+			Assert.AreEqual(
+				TimeSpan.FromHours(time).Multiply(scale),
+				TimeSpan.FromHours(time * scale));
+
+			Assert.AreEqual(
+				TimeSpan.FromDays(time).Multiply(scale),
+				TimeSpan.FromDays(time * scale));
+		}
+
+		[TestCase(16, 2)]
+		[TestCase(145, 5)]
+		[TestCase(14, -1)]
+		[TestCase(100, 0.5)]
+		[TestCase(100, -0.25)]
+		[TestCase(1, 0.125)]
+		[TestCase(11295, 125.5)]
+		public static void TestDivideSuccess(int time, double divisor)
+		{
+			Assert.AreEqual(
+				TimeSpan.FromTicks(time).Divide(divisor),
+				TimeSpan.FromTicks((long)(time / divisor)));
+
+			// Too small for timespan resolution
+			Assert.AreEqual(
+				TimeSpanHelpers.FromNanoseconds(time * 10000).Divide(divisor),
+				TimeSpanHelpers.FromNanoseconds(time * 10000 / divisor));
+
+			// Too small for timespan resolution
+			Assert.AreEqual(
+				TimeSpanHelpers.FromMicroseconds(time * 10000).Divide(divisor),
+				TimeSpanHelpers.FromMicroseconds(time * 10000 / divisor));
+
+			// HACK: see https://stackoverflow.com/a/5450495 for explanation
+			Assert.AreEqual(
+				Math.Round(TimeSpan.FromMilliseconds(time).Divide(divisor).TotalMilliseconds),
+				Math.Round(TimeSpan.FromMilliseconds(time / divisor).TotalMilliseconds));
+
+			Assert.AreEqual(
+				TimeSpan.FromSeconds(time).Divide(divisor),
+				TimeSpan.FromSeconds(time / divisor));
+
+			Assert.AreEqual(
+				TimeSpan.FromMinutes(time).Divide(divisor),
+				TimeSpan.FromMinutes(time / divisor));
+
+			Assert.AreEqual(
+				TimeSpan.FromHours(time).Divide(divisor),
+				TimeSpan.FromHours(time / divisor));
+
+			Assert.AreEqual(
+				TimeSpan.FromDays(time).Divide(divisor),
+				TimeSpan.FromDays(time / divisor));
+		}
+
+		[Test]
+		public static void TestMultiplyFail()
+		{
+			Assert.Throws<ArgumentException>(() => TimeSpan.FromDays(1).Multiply(double.NaN));
+			Assert.Throws<OverflowException>(() => TimeSpan.FromDays(1).Multiply(1e8));
+			Assert.Throws<OverflowException>(() => TimeSpan.FromDays(1).Multiply(double.NegativeInfinity));
+			Assert.Throws<OverflowException>(() => TimeSpan.FromDays(1).Multiply(double.PositiveInfinity));
+		}
+
+		[Test]
+		public static void TestDivideFail()
+		{
+			Assert.Throws<ArgumentException>(() => TimeSpan.FromDays(1).Divide(double.NaN));
+			Assert.Throws<OverflowException>(() => TimeSpan.FromDays(1).Divide(1e-8));
+		}
+
+		[TestCase(0.5)]
+		[TestCase(1)]
+		[TestCase(-100)]
+		[TestCase(123)]
+		[TestCase(0)]
+		public static void TestFromMicroseconds(double timeMs)
+		{
+			var accurateMs = TimeSpan.FromTicks((long)(timeMs * TimeSpan.TicksPerMillisecond));
+			Assert.AreEqual(
+				TimeSpanHelpers.FromMicroseconds(timeMs * 1000),
+				accurateMs);
+
+			Assert.AreEqual(
+				accurateMs.TotalMicroseconds(),
+				timeMs * 1000);
+		}
+
+		[TestCase(0.5)]
+		[TestCase(1)]
+		[TestCase(-100)]
+		[TestCase(123)]
+		[TestCase(0)]
+		public static void TestFromNanoseconds(double timeMs)
+		{
+			var accurateMs = TimeSpan.FromTicks((long)(timeMs * TimeSpan.TicksPerMillisecond));
+			Assert.AreEqual(
+				TimeSpanHelpers.FromNanoseconds(timeMs * 1000 * 1000),
+				accurateMs);
+
+			Assert.AreEqual(
+				accurateMs.TotalNanoseconds(),
+				timeMs * 1000 * 1000);
+		}
+	}
+}
---
+++ CodeJam.Main/Dates/TimeSpanHelpers.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace CodeJam.Dates
+{
+	/// <summary>
+	/// Helper methods for timespan manipulations
+	/// </summary>
+	public static class TimeSpanHelpers
+	{
+		private const long _ticksPerMicrosecond = TimeSpan.TicksPerMillisecond / 1000;
+		private const double _microsecondsPerTick = 1d / _ticksPerMicrosecond;
+		private const long _maxMicroseconds = long.MaxValue / _ticksPerMicrosecond;
+		private const long _minMicroseconds = long.MinValue / _ticksPerMicrosecond;
+
+		private const double _ticksPerNanosecond = _ticksPerMicrosecond / 1000d;
+		private const double _nanosecondsPerTick = 1d / _ticksPerNanosecond;
+		private const double _maxNanoseconds = long.MaxValue / _ticksPerNanosecond;
+		private const double _minNanoseconds = long.MinValue / _ticksPerNanosecond;
+
+		private static TimeSpan FromTicksChecked(double ticks)
+		{
+			if (double.IsNaN(ticks))
+				throw CodeExceptions.Argument(nameof(ticks), "TimeSpan does not accept floating point Not-a-Number values");
+			if (ticks <= TimeSpan.MinValue.Ticks || ticks >= TimeSpan.MaxValue.Ticks)
+				throw new OverflowException("TimeSpan overflowed because the duration is too long.");
+
+			return new TimeSpan(checked((long)ticks));
+		}
+
+		/// <summary>
+		/// Returns a TimeSpan that represents a specified number of microseconds.
+		/// </summary>
+		/// <param name="microseconds">A number of microseconds.</param>
+		/// <returns>A TimeSpan that represents value.</returns>
+		/// <exception cref="OverflowException">
+		/// value is less than <see cref="TimeSpan.MinValue"/> or greater than <see cref="TimeSpan.MaxValue"/>.
+		/// -or-
+		/// value is<see cref="double.PositiveInfinity"/>.
+		/// -or-
+		/// value is <see cref="double.NegativeInfinity"/>.
+		///</exception>
+		/// <exception cref="ArgumentException">
+		/// value is equal to <see cref="double.NaN"/>.
+		///</exception>
+		public static TimeSpan FromMicroseconds(double microseconds) =>
+			FromTicksChecked(microseconds * _ticksPerMicrosecond);
+
+		/// <summary>
+		/// Returns a TimeSpan that represents a specified number of nanoseconds.
+		/// </summary>
+		/// <param name="nanoseconds">A number of nanoseconds.</param>
+		/// <returns>A TimeSpan that represents value.</returns>
+		/// <exception cref="OverflowException">
+		/// value is less than <see cref="TimeSpan.MinValue"/> or greater than <see cref="TimeSpan.MaxValue"/>.
+		/// -or-
+		/// value is<see cref="double.PositiveInfinity"/>.
+		/// -or-
+		/// value is <see cref="double.NegativeInfinity"/>.
+		///</exception>
+		/// <exception cref="ArgumentException">
+		/// value is equal to <see cref="double.NaN"/>.
+		///</exception>
+		public static TimeSpan FromNanoseconds(double nanoseconds) =>
+			FromTicksChecked(nanoseconds * _ticksPerNanosecond);
+
+		/// <summary>
+		/// Returns a TimeSpan that represents value multiplied to specified multiplier.
+		/// </summary>
+		/// <param name="timeSpan">The time span.</param>
+		/// <param name="multiplier">The multiplier.</param>
+		/// <returns>A System.TimeSpan that represents value multiplied to specified multiplier.</returns>
+		/// <exception cref="OverflowException">value is less than <see cref="TimeSpan.MinValue" /> or greater than <see cref="TimeSpan.MaxValue" />.
+		/// -or-
+		/// value is<see cref="double.PositiveInfinity" />.
+		/// -or-
+		/// value is <see cref="double.NegativeInfinity" />.</exception>
+		/// <exception cref="ArgumentException">value is equal to <see cref="double.NaN" />.</exception>
+		public static TimeSpan Multiply(this TimeSpan timeSpan, double multiplier) =>
+			FromTicksChecked(timeSpan.Ticks * multiplier);
+
+		/// <summary>
+		/// Returns a TimeSpan that represents value divided to specified divisor.
+		/// </summary>
+		/// <param name="timeSpan">The time span.</param>
+		/// <param name="divisor">The divisor.</param>
+		/// <returns>A System.TimeSpan that represents value divided to specified divisor.</returns>
+		/// <exception cref="OverflowException">value is less than <see cref="TimeSpan.MinValue" /> or greater than <see cref="TimeSpan.MaxValue" />.
+		/// -or-
+		/// value is<see cref="double.PositiveInfinity" />.
+		/// -or-
+		/// value is <see cref="double.NegativeInfinity" />.</exception>
+		/// <exception cref="ArgumentException">value is equal to <see cref="double.NaN" />.</exception>
+		public static TimeSpan Divide(this TimeSpan timeSpan, double divisor) =>
+			FromTicksChecked(timeSpan.Ticks / divisor);
+
+		/// <summary>
+		/// Gets the value of the current TimeSpan structure expressed in whole
+		/// and fractional microseconds.
+		/// </summary>
+		/// <param name="timeSpan">The time span.</param>
+		/// <returns></returns>
+		public static double TotalMicroseconds(this TimeSpan timeSpan)
+		{
+			var temp = timeSpan.Ticks * _microsecondsPerTick;
+
+			if (temp > _maxMicroseconds)
+				return _maxMicroseconds;
+
+			if (temp < _minMicroseconds)
+				return _minMicroseconds;
+
+			return temp;
+		}
+
+		/// <summary>
+		/// Gets the value of the current TimeSpan structure expressed in whole
+		/// and fractional nanoseconds.
+		/// </summary>
+		/// <param name="timeSpan">The time span.</param>
+		/// <returns></returns>
+		public static double TotalNanoseconds(this TimeSpan timeSpan)
+		{
+			var temp = timeSpan.Ticks * _nanosecondsPerTick;
+
+			if (temp > _maxNanoseconds)
+				return _maxNanoseconds;
+
+			if (temp < _minNanoseconds)
+				return _minNanoseconds;
+
+			return temp;
+		}
+	}
+}
