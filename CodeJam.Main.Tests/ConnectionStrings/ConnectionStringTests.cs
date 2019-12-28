b9dae91ac9c9260a469c5322e4e443ef34c98eed
ig-sinicyn <igor.sinicyn@gmail.com>
2019-12-29 00:04:42 +0300

    + Connection string parser/formatter class (based on DbConnectionStringBuilder) + string.ToXxx extension methods * improve targeting for ReflectionExtensions

---
+++ CodeJam.Main.Tests/ConnectionStrings/ConnectionStringTests.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+using NUnit.Framework;
+
+using static NUnit.Framework.Assert;
+
+namespace CodeJam.ConnectionStrings
+{
+	[SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+	public class ConnectionStringTests
+	{
+		private static readonly DateTimeOffset _defaultDateTimeOffset = new DateTimeOffset(2010, 11, 12, 0, 0, 0, TimeSpan.Zero);
+
+		public class BaseConnectionString : ConnectionStringBase
+		{
+			public BaseConnectionString(string connectionString) : base(connectionString) { }
+
+#if NET35_OR_GREATER || TARGETS_NETCOREAPP
+			[Required]
+#endif
+			public string RequiredValue
+			{
+				get => TryGetValue(nameof(RequiredValue));
+				set => SetValue(nameof(RequiredValue), value);
+			}
+
+			public bool BooleanValue
+			{
+				get => TryGetBooleanValue(nameof(BooleanValue));
+				set => SetValue(nameof(BooleanValue), value);
+			}
+
+			public int? Int32Value
+			{
+				get => TryGetInt32Value(nameof(Int32Value));
+				set => SetValue(nameof(Int32Value), value);
+			}
+		}
+
+		public class DerivedConnectionString : BaseConnectionString
+		{
+			public DerivedConnectionString(string connectionString) : base(connectionString) { }
+
+			public new string RequiredValue
+			{
+				get => TryGetValue(nameof(RequiredValue));
+				set => SetValue(nameof(RequiredValue), value);
+			}
+
+			public DateTimeOffset? DateTimeOffsetValue
+			{
+				get => TryGetDateTimeOffsetValue(nameof(DateTimeOffsetValue));
+				set => SetValue(nameof(DateTimeOffsetValue), value);
+			}
+		}
+
+		public class NonBrowsableConnectionString : BaseConnectionString
+		{
+			public NonBrowsableConnectionString(string connectionString) : base(connectionString) { }
+
+			[Browsable(false)]
+			public new string RequiredValue
+			{
+				get => TryGetValue(nameof(RequiredValue));
+				set => SetValue(nameof(RequiredValue), value);
+			}
+		}
+
+		[Test]
+		public void TestConnectionStringValidation()
+		{
+			DoesNotThrow(() => new BaseConnectionString(null));
+			DoesNotThrow(() => new BaseConnectionString(""));
+			DoesNotThrow(() => new BaseConnectionString("requiredValue=aaa"));
+			DoesNotThrow(() => new BaseConnectionString("RequiredValue=aaa;IgnoredValue=123"));
+			DoesNotThrow(() => new BaseConnectionString("") { ConnectionString = null });
+			DoesNotThrow(() => new BaseConnectionString("") { ConnectionString = "" });
+			DoesNotThrow(() => new BaseConnectionString("") { ConnectionString = "requiredValue=aaa" });
+			DoesNotThrow(() => new BaseConnectionString("") { ConnectionString = "RequiredValue=aaa;IgnoredValue=123" });
+
+#if NET35_OR_GREATER || TARGETS_NETCOREAPP
+			var ex = Throws<ArgumentException>(() => new BaseConnectionString("IgnoredValue=123"));
+			That(ex.Message, Does.Contain(nameof(BaseConnectionString.RequiredValue)));
+			ex = Throws<ArgumentException>(
+				() => new BaseConnectionString("")
+				{
+					ConnectionString = "IgnoredValue = 123"
+				});
+			That(ex.Message, Does.Contain(nameof(BaseConnectionString.RequiredValue)));
+#else
+			DoesNotThrow(() => new BaseConnectionString("IgnoredValue=123"));
+			DoesNotThrow(
+				() => new BaseConnectionString("")
+				{
+					ConnectionString = "IgnoredValue = 123"
+				});;
+#endif
+		}
+
+		[Test]
+		public void TestConnectionStringValidationOverride()
+		{
+			DoesNotThrow(() => new DerivedConnectionString("IgnoredValue=123"));
+			DoesNotThrow(
+				() => new DerivedConnectionString("")
+				{
+					ConnectionString = "IgnoredValue=123"
+				});
+		}
+
+		[Test]
+		public void TestGetProperties()
+		{
+			var x = new BaseConnectionString("requiredValue=aaa");
+			AreEqual(x.RequiredValue, "aaa");
+			AreEqual(x.BooleanValue, false);
+			AreEqual(x.Int32Value, null);
+
+			IsTrue(x.ContainsKey(nameof(x.RequiredValue)));
+			IsTrue(x.TryGetValue(nameof(x.RequiredValue), out _));
+			AreEqual(x[nameof(x.RequiredValue)], "aaa");
+
+			x = new BaseConnectionString("requiredValue='aa; a'");
+			AreEqual(x.RequiredValue, "aa; a");
+
+			x = new BaseConnectionString("requiredValue=\"aa; a\"");
+			AreEqual(x.RequiredValue, "aa; a");
+
+			// test for input string format
+			x = new BaseConnectionString(@"
+	RequiredValue=""aaa"" ;
+BooleanValue=true;
+				Int32Value = 112");
+			AreEqual(x.RequiredValue, "aaa");
+			AreEqual(x.BooleanValue, true);
+			AreEqual(x.Int32Value, 112);
+		}
+
+		[Test]
+		public void TestGetPropertiesDerived()
+		{
+			var x = new DerivedConnectionString("IgnoredValue=aaa");
+			AreEqual(x.RequiredValue, null);
+			AreEqual(x.BooleanValue, false);
+			AreEqual(x.Int32Value, null);
+			AreEqual(x.DateTimeOffsetValue, null);
+
+			x = new DerivedConnectionString("DateTimeOffsetValue=2010-11-12Z");
+			AreEqual(x.DateTimeOffsetValue, _defaultDateTimeOffset);
+		}
+
+		[Test]
+		public void TestPropertiesRoundtrip()
+		{
+			var x = new DerivedConnectionString("")
+			{
+				RequiredValue = "A; B=C'\"",
+				BooleanValue = true,
+				Int32Value = -1024,
+				DateTimeOffsetValue = _defaultDateTimeOffset
+			};
+
+			var s = x.ToString();
+			AreEqual(s, @"RequiredValue=""A; B=C'"""""";DateTimeOffsetValue=""11/12/2010 00:00:00 +00:00"";BooleanValue=True;Int32Value=-1024");
+
+			var x2 = new DerivedConnectionString(s);
+			AreEqual(x2.RequiredValue, x.RequiredValue);
+			AreEqual(x2.BooleanValue, x.BooleanValue);
+			AreEqual(x2.Int32Value, x.Int32Value);
+			AreEqual(x2.DateTimeOffsetValue, x.DateTimeOffsetValue);
+			AreEqual(s, x2.ToString());
+		}
+
+		[Test]
+		public void TestIgnoredProperties()
+		{
+			var x = new DerivedConnectionString("");
+			IsFalse(x.ContainsKey("IgnoredValue"));
+			IsFalse(x.TryGetValue("IgnoredValue", out var ignored));
+			IsNull(ignored);
+			var ex = Throws<ArgumentException>(() => x["IgnoredValue"].ToString());
+			That(ex.Message, Does.Contain("IgnoredValue"));
+
+			x = new DerivedConnectionString("IgnoredValue=123");
+			IsFalse(x.ContainsKey("IgnoredValue"));
+			IsFalse(x.TryGetValue("IgnoredValue", out ignored));
+			IsNull(ignored);
+			ex = Throws<ArgumentException>(() => x["IgnoredValue"].ToString());
+			That(ex.Message, Does.Contain("IgnoredValue"));
+		}
+
+		[Test]
+		public void TestNonBrowsableProperties()
+		{
+			var x = new NonBrowsableConnectionString("")
+			{
+				RequiredValue = "A",
+				BooleanValue = true,
+				Int32Value = -1024
+			};
+
+			var s = x.GetBrowsableConnectionString(true);
+			AreEqual(s, @"RequiredValue=...;BooleanValue=True;Int32Value=-1024");
+
+			var x2 = new NonBrowsableConnectionString(s);
+			AreEqual(x2.RequiredValue, "...");
+			AreEqual(x2.BooleanValue, x.BooleanValue);
+			AreEqual(x2.Int32Value, x.Int32Value);
+			AreEqual(s, x2.ToString());
+
+			s = x.GetBrowsableConnectionString(false);
+			AreEqual(s, @"BooleanValue=True;Int32Value=-1024");
+
+			x2 = new NonBrowsableConnectionString(s);
+			AreEqual(x2.RequiredValue, null);
+			AreEqual(x2.BooleanValue, x.BooleanValue);
+			AreEqual(x2.Int32Value, x.Int32Value);
+			AreEqual(s, x2.ToString());
+		}
+	}
+}
---
+++ CodeJam.Main/ConnectionStrings/ConnectionStringBase.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+using CodeJam.Collections;
+using CodeJam.Reflection;
+using CodeJam.Strings;
+using CodeJam.Targeting;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.ConnectionStrings
+{
+	/// <summary>
+	/// Base class for connection strings
+	/// </summary>
+	[PublicAPI]
+	public abstract class ConnectionStringBase : DbConnectionStringBuilder
+	{
+		private const string _nonBrowsableValue = "...";
+
+		/// <summary>
+		/// Descriptor for connection string keyword
+		/// </summary>
+		protected class KeywordDescriptor
+		{
+			/// <summary>
+			/// Initializes a new instance of the <see cref="KeywordDescriptor"/> class.
+			/// </summary>
+			public KeywordDescriptor(string name, Type valueType, bool isRequired, bool isBrowsable)
+			{
+				Name = name;
+				ValueType = valueType;
+				IsRequired = isRequired;
+				IsBrowsable = isBrowsable;
+			}
+
+			/// <summary>
+			/// Gets the keyword name.
+			/// </summary>
+			public string Name { get; }
+
+			/// <summary>
+			/// Gets expected type of the keyword value.
+			/// </summary>
+			public Type ValueType { get; }
+
+			/// <summary>
+			/// Gets a value indicating whether this keyword is a mandatory keyword.
+			/// </summary>
+			public bool IsRequired { get; }
+
+			/// <summary>
+			/// Gets a value indicating whether this keyword is browsable (safe to log / display).
+			/// </summary>
+			public bool IsBrowsable { get; }
+		}
+
+		private static IReadOnlyDictionary<string, KeywordDescriptor> GetDescriptorsCore(Type type)
+		{
+			KeywordDescriptor GetDescriptor(PropertyInfo property) =>
+				new KeywordDescriptor(
+					property.Name,
+					property.PropertyType,
+#if NET35_OR_GREATER || TARGETS_NETSTANDARD || TARGETS_NETCOREAPP
+					property.IsRequired(),
+#else
+					false,
+#endif
+					property.IsBrowsable());
+
+			// Explicit ordering from most derived to base. Explanation:
+			// The GetProperties method does not return properties in a particular order, such as alphabetical or declaration order.
+			// Your code must not depend on the order in which properties are returned, because that order varies.
+			var typeChain = Sequence.CreateWhileNotNull(
+				type,
+				t => t.GetBaseType() is var baseType && baseType != typeof(DbConnectionStringBuilder)
+					? baseType
+					: null);
+			var properties = typeChain
+				.SelectMany(t => t.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly));
+#if NET45_OR_GREATER || TARGETS_NETSTANDARD || TARGETS_NETCOREAPP
+			var result = new Dictionary<string, KeywordDescriptor>(StringComparer.OrdinalIgnoreCase);
+#else
+			var result = new DictionaryEx<string, KeywordDescriptor>(StringComparer.OrdinalIgnoreCase);
+#endif
+			foreach (var prop in properties)
+			{
+				// DONTTOUCH: most derived property wins
+				if (result.ContainsKey(prop.Name))
+					continue;
+
+				result[prop.Name] = GetDescriptor(prop);
+			}
+			return result;
+		}
+
+		private static readonly Func<Type, IReadOnlyDictionary<string, KeywordDescriptor>> _keywordsCache = Algorithms.Memoize(
+			(Type type) => GetDescriptorsCore(type),
+			LazyThreadSafetyMode.ExecutionAndPublication);
+
+		/// <summary>Initializes a new instance of the <see cref="ConnectionStringBase" /> class.</summary>
+		/// <param name="connectionString">The connection string.</param>
+		protected ConnectionStringBase([CanBeNull] string connectionString)
+		{
+			if (connectionString != null)
+				ConnectionString = connectionString;
+		}
+
+		/// <summary>
+		/// Gets all supported keywords for current connection.
+		/// </summary>
+		[NotNull]
+		protected IReadOnlyDictionary<string, KeywordDescriptor> Keywords => _keywordsCache(GetType());
+
+		/// <summary>
+		/// Gets or sets the connection string associated with the <see cref="T:System.Data.Common.DbConnectionStringBuilder" />.
+		/// </summary>
+		[NotNull]
+		public new string ConnectionString
+		{
+			get => base.ConnectionString;
+			set
+			{
+				base.ConnectionString = value;
+				if (value.NotNullNorEmpty())
+				{
+					foreach (var nameRequiredPair in Keywords.Where(p => p.Value.IsRequired))
+					{
+						if (!ContainsKey(nameRequiredPair.Key))
+							throw CodeExceptions.Argument(
+								nameof(ConnectionString),
+								$"The value of required {nameRequiredPair.Key} connection string parameter is empty.");
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the browsable connection string.
+		/// </summary>
+		/// <param name="includeNonBrowsable">If set to <c>true</c>, non browsable values will be .</param>
+		/// <returns></returns>
+		[NotNull, MustUseReturnValue]
+		public string GetBrowsableConnectionString(bool includeNonBrowsable = false)
+		{
+			var builder = new StringBuilder();
+			foreach (var browsablePair in Keywords)
+			{
+				if (!browsablePair.Value.IsBrowsable && !includeNonBrowsable)
+					continue;
+
+				if (ShouldSerialize(browsablePair.Key) && TryGetValue(browsablePair.Key, out var value))
+				{
+					if (!browsablePair.Value.IsBrowsable)
+						value = _nonBrowsableValue;
+					var keyValue = Convert.ToString(value, CultureInfo.InvariantCulture);
+					AppendKeyValuePair(builder, browsablePair.Key, keyValue);
+				}
+			}
+
+			return builder.ToString();
+		}
+
+		#region Use only allowed keywords
+		/// <inheritdoc />
+		public override ICollection Keys => _keywordsCache(GetType()).Keys.ToArray();
+
+		/// <inheritdoc />
+		public override object this[string keyword]
+		{
+			get
+			{
+				return base[keyword];
+			}
+			set
+			{
+				if (Keywords.ContainsKey(keyword))
+					base[keyword] = value;
+			}
+		}
+		#endregion
+
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">Name of keyword</param>
+		/// <returns>Value for the keyword</returns>
+		[CanBeNull, MustUseReturnValue]
+		protected string TryGetValue(string keyword) => ContainsKey(keyword) ? (string)base[keyword] : null;
+
+		/// <summary>Set value for the keyword.</summary>
+		/// <param name="keyword">Name of keyword</param>
+		/// <param name="value">The value.</param>
+		protected void SetValue(string keyword, object value) =>
+			base[keyword] = value switch
+			{
+				DateTimeOffset x => x.ToInvariantString(),
+				Guid x => x.ToInvariantString(),
+#if NET40_OR_GREATER || TARGETS_NETSTANDARD || TARGETS_NETCOREAPP
+				TimeSpan x => x.ToInvariantString(),
+#else
+				TimeSpan x => x.ToString(),
+#endif
+				Uri x => x.ToString(),
+				_ => value
+			};
+
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">Name of keyword</param>
+		/// <returns>Value for the keyword</returns>
+		[MustUseReturnValue]
+		protected bool TryGetBooleanValue(string keyword) => ContainsKey(keyword) && Convert.ToBoolean(base[keyword]);
+
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">Name of keyword</param>
+		/// <returns>Value for the keyword</returns>
+		[CanBeNull, MustUseReturnValue]
+		protected int? TryGetInt32Value(string keyword) => ContainsKey(keyword) ? Convert.ToInt32(base[keyword]) : default(int?);
+
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">Name of keyword</param>
+		/// <returns>Value for the keyword</returns>
+		[CanBeNull, MustUseReturnValue]
+		protected long? TryGetInt64Value(string keyword) => ContainsKey(keyword) ? Convert.ToInt64(base[keyword]) : default(long?);
+
+
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">Name of keyword</param>
+		/// <returns>Value for the keyword</returns>
+		[CanBeNull, MustUseReturnValue]
+		protected DateTimeOffset? TryGetDateTimeOffsetValue(string keyword) => TryGetValue(keyword).ToDateTimeOffsetInvariant();
+
+#if NET40_OR_GREATER || TARGETS_NETSTANDARD || TARGETS_NETCOREAPP // PUBLIC_API_CHANGES
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">The value for the keyword.</param>
+		/// <returns>Value for the keyword</returns>
+		[CanBeNull, MustUseReturnValue]
+		protected Guid? TryGetGuidValue(string keyword) => TryGetValue(keyword).ToGuid();
+
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">The value for the keyword.</param>
+		/// <returns>Value for the keyword</returns>
+		[CanBeNull, MustUseReturnValue]
+		protected TimeSpan? TryGetTimeSpanValue(string keyword) => TryGetValue(keyword).ToTimeSpanInvariant();
+
+		/// <summary>Gets the value for the keyword.</summary>
+		/// <param name="keyword">The value for the keyword.</param>
+		/// <returns>Value for the keyword.</returns>
+		[CanBeNull, MustUseReturnValue]
+		protected Uri TryGetUriValue(string keyword) => TryGetValue(keyword).ToUri();
+#endif
+	}
+}
