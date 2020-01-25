e580aa277fb3a8827ba4a110dced65f6eb8773d2
ig-sinicyn <igor.sinicyn@gmail.com>
2020-01-25 14:44:36 +0300

    + Algorithms.MinMax

---
+++ CodeJam.Main.Tests/Algorithms/MinMaxTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace CodeJam
+{
+	[TestFixture]
+	public class MinMaxTest
+	{
+		public struct Comparable : IComparable
+		{
+			public Comparable(int value)
+			{
+				Value = value;
+			}
+
+			public int Value { get; }
+
+			#region Implementation of IComparable
+			public int CompareTo(object obj)
+			{
+				if (!(obj is Comparable other)) return 1;
+
+				return Value.CompareTo(other.Value);
+			}
+			#endregion
+		}
+
+		[Test]
+		public static void TestMinMaxTrivial()
+		{
+			var c1 = 1M;
+			var c2 = 2M;
+
+			Assert.AreEqual(c1, Algorithms.Min(c1, c2));
+			Assert.AreEqual(c1, Algorithms.Min(c2, c1));
+			Assert.AreEqual(c2, Algorithms.Max(c1, c2));
+			Assert.AreEqual(c2, Algorithms.Max(c2, c1));
+		}
+
+		[Test]
+		public static void TestMinMaxOverIComparable()
+		{
+			var c1 = new Comparable(1);
+			var c2 = new Comparable(2);
+
+			Assert.AreEqual(c1, Algorithms.Min(c1, c2));
+			Assert.AreEqual(c1, Algorithms.Min(c2, c1));
+			Assert.AreEqual(c2, Algorithms.Max(c1, c2));
+			Assert.AreEqual(c2, Algorithms.Max(c2, c1));
+		}
+
+		[Test]
+		public static void TestMinMaxOverNullable()
+		{
+			Comparable? c1 = new Comparable(1);
+			Comparable? c2 = new Comparable(2);
+
+			Assert.AreEqual(c1, Algorithms.Min(c1, c2));
+			Assert.AreEqual(c1, Algorithms.Min(c2, c1));
+			Assert.AreEqual(c2, Algorithms.Max(c1, c2));
+			Assert.AreEqual(c2, Algorithms.Max(c2, c1));
+
+			Assert.AreEqual(null, Algorithms.Min(c1, null));
+			Assert.AreEqual(null, Algorithms.Min(null, c1));
+			Assert.AreEqual(c1, Algorithms.Max(null, c1));
+			Assert.AreEqual(c1, Algorithms.Max(c1, null));
+		}
+
+		[Test]
+		public static void TestMinMaxOverNullableEnum()
+		{
+			ConsoleColor? c1 = (ConsoleColor)1;
+			ConsoleColor? c2 = (ConsoleColor)2;
+
+			Assert.AreEqual(c1, Algorithms.Min(c1, c2));
+			Assert.AreEqual(c1, Algorithms.Min(c2, c1));
+			Assert.AreEqual(c2, Algorithms.Max(c1, c2));
+			Assert.AreEqual(c2, Algorithms.Max(c2, c1));
+
+			Assert.AreEqual(null, Algorithms.Min(c1, null));
+			Assert.AreEqual(null, Algorithms.Min(null, c1));
+			Assert.AreEqual(c1, Algorithms.Max(null, c1));
+			Assert.AreEqual(c1, Algorithms.Max(c1, null));
+		}
+	}
+}
---
+++ CodeJam.Main/Algorithms/Algorithms.MinMax.cs
@@ -0,0 +1,26 @@
+﻿
+using CodeJam.Arithmetic;
+
+namespace CodeJam
+{
+	/// <summary>Miscellaneous algorithms</summary>
+	partial class Algorithms
+	{
+		/// <summary>
+		/// Compares two values and returns minimal of them
+		/// </summary>
+		/// <typeparam name="T">Type of the values.</typeparam>
+		/// <param name="value1">Value 1.</param>
+		/// <param name="value2">Value 2.</param>
+		/// <returns>Minimal value.</returns>
+		public static T Min<T>(T value1, T value2) => Operators<T>.Compare(value1, value2) <= 0 ? value1 : value2;
+
+		/// <summary>
+		/// Compares two values and returns minimal of them
+		/// </summary>
+		/// <param name="value1">Value 1.</param>
+		/// <param name="value2">Value 2.</param>
+		/// <returns>Maximum value.</returns>
+		public static T Max<T>(T value1, T value2) => Operators<T>.Compare(value1, value2) >= 0 ? value1 : value2;
+	}
+}
