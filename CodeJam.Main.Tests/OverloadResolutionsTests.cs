57985cd16e704b8c2831206b76badcf3e3dc687b
ig-sinicyn <igor.sinicyn@gmail.com>
2020-02-16 19:03:42 +0300

    * Fix overload resolution on array.Reverse() * Update packages * Fix indexed item equality comparison (item index was ignored).

---
+++ CodeJam.Main.Tests/OverloadResolutionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+using CodeJam.Collections;
+
+using NUnit.Framework;
+
+// ReSharper disable once CheckNamespace
+namespace CodeJam
+{
+	[TestFixture(Category = "Compatibility")]
+	public static class OverloadResolutionsTests
+	{
+		[Test]
+		public static void ArrayResolutionTests()
+		{
+			var a = new[] { 1, 2, 3 };
+			a.Reverse();
+			Assert.AreEqual(a, new[] { 3, 2, 1 });
+		}
+	}
+}
