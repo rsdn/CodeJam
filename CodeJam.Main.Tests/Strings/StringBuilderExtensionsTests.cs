821e49f76c1c392992fc7e0d47878ee1a2b7d683
NN <nn1436401@gmail.com>
2020-06-05 22:57:05 +0300

    Add StringBuilder.IsNullOrEmpty and StringBuilder.NotNullNorEmpty.

---
+++ CodeJam.Main.Tests/Strings/StringBuilderExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+using NUnit.Framework;
+
+namespace CodeJam.Strings
+{
+	[TestFixture]
+	public class StringBuilderExtensionsTests
+	{
+		[Test]
+		public void IsNullOrEmptyConstructor()
+		{
+			Assert.IsTrue(new StringBuilder().IsNullOrEmpty());
+		}
+
+		[TestCase(null, ExpectedResult = true)]
+		[TestCase("", ExpectedResult = true)]
+		[TestCase("abc", ExpectedResult = false)]
+		public bool IsNullOrEmpty(string source) => new StringBuilder(source).IsNullOrEmpty();
+
+		[Test]
+		public void NotNullNorEmptyConstructor()
+		{
+			Assert.IsFalse(new StringBuilder().NotNullNorEmpty());
+		}
+
+		[TestCase(null, ExpectedResult = false)]
+		[TestCase("", ExpectedResult = false)]
+		[TestCase("abc", ExpectedResult = true)]
+		public bool NotNullNorEmpty(string source) => new StringBuilder(source).NotNullNorEmpty();
+	}
+}
