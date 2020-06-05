67d859224382a49b9d5097074d9e4d7439d03ea2
NN <nn1436401@gmail.com>
2020-06-05 18:17:37 +0300

    Add StringBuilder.IsNullOrEmpty and StringBuilder.NotNullNorEmpty.

---
+++ CodeJam.Main/Strings/StringBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+using JetBrains.Annotations;
+
+namespace CodeJam.Strings
+{
+	/// <summary>
+	/// <see cref="StringBuilder"/> class extensions.
+	/// </summary>
+	[PublicAPI]
+	public static class StringBuilderExtensions
+	{
+		/// <summary>
+		/// Determines whether the StringBuilder is null or empty.
+		/// </summary>
+		/// <param name="value">The StringBuilder to test.</param>
+		/// <returns>
+		/// <c>true</c> if <paramref name="value"/> is null or empty; otherwise, <c>false</c>.
+		/// </returns>
+		[Pure]
+ 		[ContractAnnotation("value:null => true")]
+		public static bool IsNullOrEmpty([CanBeNull] this StringBuilder value) =>
+			value == null || value.Length == 0;
+
+		/// <summary>
+		/// Determines whether the StringBuilder is neither null nor empty.
+		/// </summary>
+		/// <param name="value">The StringBuilder to test.</param>
+		/// <returns>
+		/// <c>true</c> if <paramref name="value"/> is not null and not empty; otherwise, <c>false</c>.
+		/// </returns>
+		[Pure]
+		[ContractAnnotation("value:null => false")]
+		public static bool NotNullNorEmpty([CanBeNull] this StringBuilder value) =>
+			!IsNullOrEmpty(value);
+	}
+}
