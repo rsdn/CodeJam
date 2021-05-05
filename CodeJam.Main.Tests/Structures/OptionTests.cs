b9a59321bfc3ac2d20c3dac59cd266c87655adaf
NN <NN---@users.noreply.github.com>
2021-05-05 14:58:39 +0300

    Add tests to Option and ValueOption.

---
+++ CodeJam.Main.Tests/Structures/OptionTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace CodeJam.Structures
+{
+	[TestFixture]
+	public class OptionTests
+	{
+		[Test]
+		public void SomeTest()
+		{
+			var some = Option.Some(1);
+			Assert.IsTrue(some.HasValue);
+			Assert.AreEqual(some.Value, 1);
+			Assert.AreEqual(some.GetValueOrDefault(2), 1);
+			Assert.AreEqual(some.GetValueOrDefault(value => 10, () => 20), 10);
+		}
+
+		[Test]
+		public void NoneTest()
+		{
+			var none = Option.None<int>();
+			Assert.IsFalse(none.HasValue);
+			Assert.AreEqual(none.GetValueOrDefault(2), 2);
+			Assert.AreEqual(none.GetValueOrDefault(value => 10, () => 20), 20);
+		}
+	}
+}
